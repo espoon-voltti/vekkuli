4726927e29aed2deb7989e3e5b53e675b24b9ce4
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-06-05 08:46:32 +0300

    Add assertions for validation errors

--- service/src/e2eTest/kotlin/fi/espoo/vekkuli/BoatSpaceApplicationTests.kt
+++ service/src/e2eTest/kotlin/fi/espoo/vekkuli/BoatSpaceApplicationTests.kt
@@ -18,6 +18,21 @@ class BoatSpaceApplicationTests : PlaywrightTest() {
 
         val applicationPage = BoatSpaceApplicationPage(page)
         applicationPage.navigateTo()
+
+        applicationPage.submitButton.click()
+
+        assertThat(applicationPage.nameRequiredError).isVisible()
+        assertThat(applicationPage.emailRequiredError).isVisible()
+        assertThat(applicationPage.phoneRequiredError).isVisible()
+        assertThat(applicationPage.boatTypeRequiredError).isVisible()
+        assertThat(applicationPage.boatNameRequiredError).isVisible()
+        assertThat(applicationPage.registrationCodeRequiredError).isVisible()
+        assertThat(applicationPage.lengthRequiredError).isVisible()
+        assertThat(applicationPage.widthRequiredError).isVisible()
+        assertThat(applicationPage.weightRequiredError).isVisible()
+        assertThat(applicationPage.boatSpaceTypeRequiredError).isVisible()
+        assertThat(applicationPage.locationRequiredError).isVisible()
+
         applicationPage.nameField.fill("Testi")
         applicationPage.emailField.fill("test@test.com")
         applicationPage.phoneField.fill("1234567890")
@@ -29,6 +44,7 @@ class BoatSpaceApplicationTests : PlaywrightTest() {
         applicationPage.weightField.fill("100")
         applicationPage.boatSpaceTypeSelect.selectOption("Harbor space")
         applicationPage.locationSelect.selectOption("Kivenlahti")
+
         applicationPage.submitButton.click()
 
         assertThat(page.getByText("Application received")).isVisible()
--- service/src/e2eTest/kotlin/fi/espoo/vekkuli/pages/BoatSpaceApplicationPage.kt
+++ service/src/e2eTest/kotlin/fi/espoo/vekkuli/pages/BoatSpaceApplicationPage.kt
@@ -9,16 +9,39 @@ import fi.espoo.vekkuli.baseUrl
 
 class BoatSpaceApplicationPage(private val page: Page) {
     val navigateTo = { page.navigate("$baseUrl/venepaikkahakemus") }
+
     val nameField = page.getByTestId("name")
+    val nameRequiredError = page.getByTestId("name-error")
+
     val emailField = page.getByTestId("email")
+    val emailRequiredError = page.getByTestId("email-error")
+
     val phoneField = page.getByTestId("phone")
+    val phoneRequiredError = page.getByTestId("phone-error")
+
     val boatTypeSelect = page.getByTestId("boatType")
+    val boatTypeRequiredError = page.getByTestId("boatType-error")
+
     val boatNameField = page.getByTestId("boatName")
+    val boatNameRequiredError = page.getByTestId("boatName-error")
+
     val registrationCodeField = page.getByTestId("registrationCode")
+    val registrationCodeRequiredError = page.getByTestId("registrationCode-error")
+
     val lengthField = page.getByTestId("length")
+    val lengthRequiredError = page.getByTestId("length-error")
+
     val widthField = page.getByTestId("width")
+    val widthRequiredError = page.getByTestId("width-error")
+
     val weightField = page.getByTestId("weight")
+    val weightRequiredError = page.getByTestId("weight-error")
+
     val boatSpaceTypeSelect = page.getByTestId("boatSpaceType")
+    val boatSpaceTypeRequiredError = page.getByTestId("boatSpaceType-error")
+
     val locationSelect = page.getByTestId("locationId")
+    val locationRequiredError = page.getByTestId("locationId-error")
+
     val submitButton = page.getByTestId("submit")
 }
