c3a9430da55ecf352d9d8536132ca9f20aac946e
merisaar <merisrnn@gmail.com>
2024-06-05 14:08:37 +0300

    remove unnecessary trailer space table

--- service/src/main/resources/db/migration/V002__add_trailer_space.sql
+++ service/src/main/resources/db/migration/V002__add_trailer_space.sql
@@ -1,11 +1,3 @@
-CREATE TABLE trailer_space
-(
-    id     Serial PRIMARY KEY,
-    width  int NOT NULL,
-    length int NOT NULL,
-    registration_number text NOT NULL
-);
-
 ALTER TABLE boat_space_application
     ADD trailer_space_width int,
     ADD trailer_space_length int,
