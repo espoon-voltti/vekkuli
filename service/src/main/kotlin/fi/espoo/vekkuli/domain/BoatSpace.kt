a8ae998f0f04a6479aea1a8ed64de84aaedaf453
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-06-17 09:03:14 +0300

    wip

--- service/src/main/kotlin/fi/espoo/vekkuli/domain/BoatSpace.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/domain/BoatSpace.kt
@@ -22,6 +22,7 @@ enum class BoatSpaceType {
 }
 
 data class BoatSpace(
+    val id: Int,
     val type: BoatSpaceType,
     val section: String,
     val placeNumber: Int,
@@ -104,7 +105,8 @@ data class BoatSpaceGroup(
     val width_cm: Int,
     val count: Int,
     val amenity: BoatSpaceAmenity,
-    val price: Int
+    val price: Int,
+    val type: BoatSpaceType
 )
 
 fun Handle.getBoatSpaceGroups(
@@ -115,7 +117,7 @@ fun Handle.getBoatSpaceGroups(
 ): List<BoatSpaceGroup> {
     val sql =
         """
-        SELECT location.name as location_name, section, length_cm, width_cm, COUNT(*) as count, amenity, price.price as price
+        SELECT location.name as location_name, section, length_cm, width_cm, COUNT(*) as count, amenity, price.price as price, boat_space.type as type 
         FROM boat_space
         JOIN location
         ON location_id = location.id
@@ -126,7 +128,7 @@ fun Handle.getBoatSpaceGroups(
             ${if (length != null) "AND length_cm >= :minLength AND length_cm <= :maxLength" else ""}
             ${if (!amenities.isNullOrEmpty()) "AND amenity IN (<amenities>)" else ""}
             ${if (boatSpaceType != null) "AND type = :boatSpaceType" else ""}
-        GROUP BY location.name, section, length_cm, width_cm, amenity, price
+        GROUP BY boat_space.type, location.name, section, length_cm, width_cm, amenity, price
         ORDER BY price 
         LIMIT 10
         """.trimIndent()
@@ -150,7 +152,7 @@ fun Handle.getBoatSpaceGroups(
     return query.mapTo<BoatSpaceGroup>().toList()
 }
 
-fun Handle.getHarbors(
+fun Handle.getUnreservedBoatSpaceOptions(
     width: Int? = null,
     length: Int? = null,
     amenities: List<BoatSpaceAmenity>? = null,
@@ -158,7 +160,7 @@ fun Handle.getHarbors(
 ): List<Harbor> {
     val sql =
         """
-        SELECT location.id as location_id, location.name as location_name, section, length_cm, width_cm, COUNT(*) as count, amenity, price.price as price
+        SELECT location.id as location_id,type , location.name as location_name, section, length_cm, width_cm, COUNT(*) as count, amenity, price.price as price
         FROM boat_space
         JOIN location
         ON location_id = location.id
@@ -169,7 +171,7 @@ fun Handle.getHarbors(
             ${if (length != null) "AND length_cm >= :minLength AND length_cm <= :maxLength" else ""}
             ${if (!amenities.isNullOrEmpty()) "AND amenity IN (<amenities>)" else ""}
             ${if (boatSpaceType != null) "AND type = :boatSpaceType" else ""}
-        GROUP BY location.id, location.name, section, length_cm, width_cm, amenity, price
+        GROUP BY location.id, location.name, type, section, length_cm, width_cm, amenity, price
         ORDER BY price 
         """.trimIndent()
 
@@ -205,7 +207,8 @@ fun Handle.getHarbors(
                     width_cm = rs.getInt("width_cm"),
                     count = rs.getInt("count"),
                     amenity = BoatSpaceAmenity.valueOf(rs.getString("amenity")),
-                    price = rs.getInt("price")
+                    price = rs.getInt("price"),
+                    type = BoatSpaceType.valueOf(rs.getString("type"))
                 )
             Pair(location, boatSpaceGroup)
         }.toList()
@@ -218,4 +221,34 @@ fun Handle.getHarbors(
             }
 
     return harbors
+}
+
+fun Handle.getUnreservedBoatSpace(
+    width: Int,
+    length: Int,
+    amenity: BoatSpaceAmenity,
+    boatSpaceType: BoatSpaceType,
+    section: String
+): BoatSpace? {
+    val sql =
+        """
+        SELECT boat_space.*, location.name as location_name, location.id as location_id, COUNT(*) OVER() AS total_count
+        FROM boat_space
+        LEFT JOIN location
+        ON location_id = location.id
+        WHERE 1=1
+            AND width_cm = :width
+            AND length_cm = :length
+            AND amenity = :amenity
+            AND type = :boatSpaceType
+            AND section = :section
+        """.trimIndent()
+    val query = createQuery(sql)
+    query.bind("width", width)
+    query.bind("length", length)
+    query.bind("amenity", amenity)
+    query.bind("boatSpaceType", boatSpaceType)
+    query.bind("section", section)
+
+    return query.mapTo<BoatSpace>().first()
 }
