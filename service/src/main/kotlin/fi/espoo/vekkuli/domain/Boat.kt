a93abd64a69c68fe952c1b718b48458688594224
Jukka Aittokallio <jai@reaktor.fi>
2024-07-30 12:29:59 +0300

    user can get information from an existing boat he owns (#97)
    * user can select previously owned boat
    * Update boat if using existing information

--- service/src/main/kotlin/fi/espoo/vekkuli/domain/Boat.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/domain/Boat.kt
@@ -34,6 +34,63 @@ enum class OwnershipStatus {
     FutureOwner
 }
 
+fun Handle.getBoatsForCitizen(citizenId: UUID): List<Boat> {
+    val query =
+        createQuery(
+            """
+            SELECT * FROM boat
+            WHERE citizen_id = :citizenId
+            """.trimIndent()
+        )
+    query.bind("citizenId", citizenId)
+    return query.mapTo<Boat>().list()
+}
+
+fun Handle.getBoat(boatId: Int): Boat? {
+    val query =
+        createQuery(
+            """
+            SELECT * FROM boat
+            WHERE id = :id
+            """.trimIndent()
+        )
+    query.bind("id", boatId)
+    return query.mapTo<Boat>().findOne().orElse(null)
+}
+
+fun Handle.updateBoat(boat: Boat): Boat {
+    val query =
+        createQuery(
+            """
+            UPDATE boat
+            SET registration_code = :registrationCode, 
+                name = :name, 
+                width_cm = :widthCm, 
+                length_cm = :lengthCm, 
+                depth_cm = :depthCm, 
+                weight_kg = :weightKg, 
+                type = :type, 
+                other_identification = :otherIdentification, 
+                extra_information = :extraInformation, 
+                ownership = :ownership
+            WHERE id = :id
+            RETURNING *
+            """.trimIndent()
+        )
+    query.bind("id", boat.id)
+    query.bind("registrationCode", boat.registrationCode)
+    query.bind("name", boat.name)
+    query.bind("widthCm", boat.widthCm)
+    query.bind("lengthCm", boat.lengthCm)
+    query.bind("depthCm", boat.depthCm)
+    query.bind("weightKg", boat.weightKg)
+    query.bind("type", boat.type)
+    query.bind("otherIdentification", boat.otherIdentification)
+    query.bind("extraInformation", boat.extraInformation)
+    query.bind("ownership", boat.ownership)
+    return query.mapTo<Boat>().one()
+}
+
 fun Handle.insertBoat(
     citizenId: UUID,
     registrationCode: String,
