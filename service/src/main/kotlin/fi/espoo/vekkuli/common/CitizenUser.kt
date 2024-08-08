00a0908f134f2db46916842e10955b9cec9f270b
merisaar <merisrnn@gmail.com>
2024-08-08 16:06:51 +0300

    allow null address

--- service/src/main/kotlin/fi/espoo/vekkuli/common/CitizenUser.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/common/CitizenUser.kt
@@ -10,8 +10,8 @@ import org.jdbi.v3.core.kotlin.bindKotlin
 import org.jdbi.v3.core.kotlin.mapTo
 
 data class LocalizedName(
-    val sv: String,
-    val fi: String
+    val sv: String?,
+    val fi: String?
 )
 
 data class CitizenAdUser(
@@ -21,22 +21,38 @@ data class CitizenAdUser(
     val email: String?,
     val phone: String?,
     val address: LocalizedName?,
-    val postalCode: String? = null,
+    val postalCode: String?,
     val town: LocalizedName?,
 )
 
-fun Handle.upsertCitizenUserFromAd(adUser: CitizenAdUser): Citizen =
-    createQuery(
+fun Handle.upsertCitizenUserFromAd(adUser: CitizenAdUser): Citizen {
+    var params =
+        adUser.copy(
+            // ensures that fields are set to null if the object is null
+            address =
+                adUser.address?.let { LocalizedName(it.fi.orEmpty(), it.sv.orEmpty()) } ?: LocalizedName(
+                    null,
+                    null
+                ),
+            town =
+                adUser.town?.let { LocalizedName(it.fi.orEmpty(), it.sv.orEmpty()) } ?: LocalizedName(
+                    null,
+                    null
+                ),
+        )
+
+    return createQuery(
         // language=SQL
         """
  INSERT INTO citizen (national_id, first_name, last_name, phone, email, address, postal_code, municipality)
- VALUES (:nationalId, :firstName, :lastName, COALESCE(:phone, ''), COALESCE(:email, ''), :address.fi, :postalCode, :town.fi)
+ VALUES (:nationalId, :firstName, :lastName, COALESCE(:phone, ''), COALESCE(:email, ''), COALESCE(:address.fi, ''), COALESCE(:postalCode, ''), COALESCE(:town.fi, ''))
  ON CONFLICT (national_id) DO UPDATE
  SET updated = now(), first_name = :firstName, last_name = :lastName, email = COALESCE(:email, ''), phone = COALESCE(:phone, '')
  RETURNING *
     """
             .trimIndent()
     )
-        .bindKotlin(adUser)
+        .bindKotlin(params)
         .mapTo<Citizen>()
         .one()
+}
