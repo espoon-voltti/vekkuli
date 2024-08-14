c70820cd39c423f524864ccdb5a79a4d2668bfc4
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-08-14 09:29:11 +0300

    Layout with login (#131)

---
+++ service/src/main/kotlin/fi/espoo/vekkuli/controllers/GlobalControllerAdvice.kt
@@ -0,0 +1,50 @@
+package fi.espoo.vekkuli.controllers
+
+import fi.espoo.vekkuli.common.getAppUser
+import fi.espoo.vekkuli.config.getAuthenticatedUser
+import fi.espoo.vekkuli.domain.getCitizen
+import jakarta.servlet.http.HttpServletRequest
+import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.kotlin.inTransactionUnchecked
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ModelAttribute
+
+@ControllerAdvice
+class GlobalControllerAdvice
+    @Autowired
+    constructor(private val jdbi: Jdbi) {
+        @ModelAttribute
+        fun addAttributes(
+            model: Model,
+            request: HttpServletRequest
+        ) {
+            val authenticatedUser = request.getAuthenticatedUser()
+            model.addAttribute("isAuthenticated", authenticatedUser != null)
+
+            if (authenticatedUser?.type == "citizen") {
+                val user =
+                    authenticatedUser.let {
+                        jdbi.inTransactionUnchecked { tx ->
+                            tx.getCitizen(authenticatedUser.id)
+                        }
+                    }
+
+                if (user != null) {
+                    model.addAttribute("userName", "${user.firstName} ${user.lastName}")
+                }
+            } else if (authenticatedUser?.type == "employee") {
+                val user =
+                    authenticatedUser.let {
+                        jdbi.inTransactionUnchecked { tx ->
+                            tx.getAppUser(authenticatedUser.id)
+                        }
+                    }
+
+                if (user != null) {
+                    model.addAttribute("userName", "${user.firstName} ${user.lastName}")
+                }
+            }
+        }
+    }
--- service/src/main/kotlin/fi/espoo/vekkuli/controllers/HomeController.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/controllers/HomeController.kt
@@ -1,15 +1,7 @@
-// SPDX-FileCopyrightText: 2023-2024 City of Espoo
-//
-// SPDX-License-Identifier: LGPL-2.1-or-later
-
 package fi.espoo.vekkuli.controllers
 
-import fi.espoo.vekkuli.common.getAppUser
-import fi.espoo.vekkuli.config.getAuthenticatedUser
-import fi.espoo.vekkuli.domain.getCitizen
 import jakarta.servlet.http.HttpServletRequest
 import org.jdbi.v3.core.Jdbi
-import org.jdbi.v3.core.kotlin.inTransactionUnchecked
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -25,13 +17,6 @@ class HomeController {
         request: HttpServletRequest,
         model: Model
     ): String {
-        val authenticatedUser = request.getAuthenticatedUser()
-        val user = authenticatedUser?.let { jdbi.inTransactionUnchecked { tx -> tx.getCitizen(it.id) } }
-        val isAuthenticated = user != null
-        model.addAttribute("isAuthenticated", isAuthenticated)
-        if (user != null) {
-            model.addAttribute("userName", "${user.firstName} ${user.lastName}")
-        }
         return "citizen-home"
     }
 
@@ -40,13 +25,6 @@ class HomeController {
         request: HttpServletRequest,
         model: Model
     ): String {
-        val authenticatedUser = request.getAuthenticatedUser()
-        val user = authenticatedUser?.let { jdbi.inTransactionUnchecked { tx -> tx.getAppUser(it.id) } }
-        val isAuthenticated = user != null
-        model.addAttribute("isAuthenticated", isAuthenticated)
-        if (user != null) {
-            model.addAttribute("userName", "${user.firstName} ${user.lastName}")
-        }
         return "home"
     }
 }
