b358176838bb9a5a0f45897edcb75f21268821ea
merisaar <merisrnn@gmail.com>
2024-08-15 15:32:05 +0300

    add payment page

--- service/src/main/kotlin/fi/espoo/vekkuli/controllers/PaymentController.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/controllers/PaymentController.kt
@@ -4,6 +4,7 @@ import fi.espoo.vekkuli.config.Paytrail
 import fi.espoo.vekkuli.config.PaytrailCustomer
 import fi.espoo.vekkuli.config.PaytrailPaymentParams
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/maksut")
 class PaymentController {
     @GetMapping("/maksa")
-    suspend fun payment(): String {
+    suspend fun payment(model: Model): String {
         val response =
             Paytrail.createPayment(
                 PaytrailPaymentParams(
@@ -22,7 +23,8 @@ class PaymentController {
                     customer = PaytrailCustomer(email = "test.customer@example.com", firstName = null, lastName = null, phone = null),
                 )
             )
-        println(response)
-        return "payment"
+
+        model.addAttribute("providers", response.providers)
+        return "boat-space-reservation-payment"
     }
 }
