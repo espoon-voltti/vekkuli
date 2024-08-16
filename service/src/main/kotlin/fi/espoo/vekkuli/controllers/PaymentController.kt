5d4660d607a702e7fe35389020c2e1221b5a895e
Meri Saarinen <merisrnn@gmail.com>
2024-08-16 12:37:19 +0300

    Payment page (#153)
    * add payment page
    
    * redirect to payment page
    
    * fix e2e test

--- service/src/main/kotlin/fi/espoo/vekkuli/controllers/PaymentController.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/controllers/PaymentController.kt
@@ -3,15 +3,36 @@ package fi.espoo.vekkuli.controllers
 import fi.espoo.vekkuli.config.Paytrail
 import fi.espoo.vekkuli.config.PaytrailCustomer
 import fi.espoo.vekkuli.config.PaytrailPaymentParams
+import fi.espoo.vekkuli.controllers.Utils.Companion.getCitizen
+import fi.espoo.vekkuli.controllers.Utils.Companion.redirectUrl
+import jakarta.servlet.http.HttpServletRequest
+import org.jdbi.v3.core.Jdbi
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+enum class PaymentType {
+    BOAT_SPACE_RESERVATION
+}
 
 @Controller
-@RequestMapping("/maksut")
+@RequestMapping("/kuntalainen/maksut")
 class PaymentController {
+    @Autowired
+    lateinit var jdbi: Jdbi
+
     @GetMapping("/maksa")
-    suspend fun payment(): String {
+    suspend fun payment(
+        @RequestParam id: String,
+        @RequestParam type: PaymentType,
+        model: Model,
+        request: HttpServletRequest
+    ): String {
+        val citizen = getCitizen(request, jdbi) ?: return redirectUrl("/")
+
         val response =
             Paytrail.createPayment(
                 PaytrailPaymentParams(
@@ -19,10 +40,17 @@ class PaymentController {
                     reference = Math.random().toString(),
                     amount = 1525,
                     language = "FI",
-                    customer = PaytrailCustomer(email = "test.customer@example.com", firstName = null, lastName = null, phone = null),
+                    customer =
+                        PaytrailCustomer(
+                            email = citizen.email,
+                            firstName = citizen.firstName,
+                            lastName = citizen.lastName,
+                            phone = citizen.phone
+                        ),
                 )
             )
-        println(response)
-        return "payment"
+
+        model.addAttribute("providers", response.providers)
+        return "boat-space-reservation-payment"
     }
 }
