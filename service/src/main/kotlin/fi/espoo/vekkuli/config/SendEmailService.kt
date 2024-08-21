95c4dbe51e301f92470266922b28f733f862424c
Jukka Aittokallio <jai@reaktor.fi>
2024-08-21 11:57:04 +0300

    fix reservation end date

---
+++ service/src/main/kotlin/fi/espoo/vekkuli/config/SendEmailService.kt
@@ -0,0 +1,48 @@
+package fi.espoo.vekkuli.config
+
+import org.springframework.stereotype.Service
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.ses.SesClient
+import software.amazon.awssdk.services.ses.model.*
+
+@Service
+class SendEmailService {
+    fun sendEmail(
+        sender: String,
+        recipient: String,
+        subject: String,
+        message: String
+    ): String? {
+        val sesClient =
+            SesClient
+                .builder()
+                .region(Region.EU_WEST_1)
+                .build()
+
+        val emailRequest =
+            SendEmailRequest
+                .builder()
+                .destination(Destination.builder().toAddresses(recipient).build())
+                .message(
+                    Message
+                        .builder()
+                        .subject(Content.builder().data(subject).build())
+                        .body(
+                            Body
+                                .builder()
+                                .text(Content.builder().data(message).build())
+                                .build()
+                        ).build()
+                ).source(sender)
+                .build()
+        try {
+            val response = sesClient.sendEmail(emailRequest)
+            return response.messageId()
+        } catch (ex: SesException) {
+            println("Failed to send email: ${ex.awsErrorDetails().errorMessage()}")
+            return null
+        } finally {
+            sesClient.close()
+        }
+    }
+}
