856aa7c7868915db9c10dfa01060d9018c9d7904
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-08-21 12:53:08 +0300

    Added validation for one field

--- service/src/main/kotlin/fi/espoo/vekkuli/controllers/CitizenUserController.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/controllers/CitizenUserController.kt
@@ -1,16 +1,22 @@
 package fi.espoo.vekkuli.controllers
 
+import fi.espoo.vekkuli.config.MessageUtil
 import fi.espoo.vekkuli.domain.BoatSpaceReservation
 import fi.espoo.vekkuli.domain.Citizen
 import fi.espoo.vekkuli.domain.getBoatsForCitizen
 import jakarta.servlet.http.HttpServletRequest
 import org.jdbi.v3.core.Jdbi
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
 import java.util.*
 
 @Controller
@@ -18,6 +24,9 @@ import java.util.*
 class CitizenUserController {
     @Autowired
     lateinit var jdbi: Jdbi
+
+    @Autowired
+    lateinit var messageUtil: MessageUtil
 
     @GetMapping("/kayttaja/{citizenId}")
     fun boatSpaceSearchPage(
@@ -45,6 +54,39 @@ class CitizenUserController {
         val boats = getBoatsForCitizen(citizenId, jdbi)
         val boat = boats.find { it.id == boatId } ?: throw IllegalArgumentException("Boat not found")
         model.addAttribute("boat", boat)
+        model.addAttribute("errors", mutableMapOf<String, String>())
         return "employee/edit-boat"
+    }
+
+    data class BoatUpdateInput(val name: String)
+
+    fun validateBoatUpdateInput(input: BoatUpdateInput): MutableMap<String, String> {
+        val errors = mutableMapOf<String, String>()
+        if (input.name.isBlank()) {
+            errors["name"] = messageUtil.getMessage("validation.required")
+        }
+        return errors
+    }
+
+    @PatchMapping("/kayttaja/{citizenId}/vene/{boatId}")
+    fun updateBoat(
+        request: HttpServletRequest,
+        @PathVariable citizenId: UUID,
+        @PathVariable boatId: Int,
+        input: BoatUpdateInput,
+        model: Model
+    ): Any {
+        val boats = getBoatsForCitizen(citizenId, jdbi)
+        val boat = boats.find { it.id == boatId } ?: throw IllegalArgumentException("Boat not found")
+        model.addAttribute("boat", boat)
+        val errors = validateBoatUpdateInput(input)
+        if (errors.isNotEmpty()) {
+            model.addAttribute("errors", errors)
+            return ModelAndView("employee/edit-boat", model.asMap(), HttpStatus.OK)
+        }
+        val headers = HttpHeaders()
+        headers.add("HX-Redirect", "/virkailija/kayttaja/$citizenId")
+
+        return ResponseEntity<Any>(headers, HttpStatus.OK)
     }
 }
