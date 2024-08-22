662eec9176cf22b6069bcb88423a5b8569b7fbb4
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-08-22 14:19:09 +0300

    Add validations for the number fields

--- service/src/main/kotlin/fi/espoo/vekkuli/controllers/CitizenUserController.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/controllers/CitizenUserController.kt
@@ -1,16 +1,21 @@
 package fi.espoo.vekkuli.controllers
 
+import fi.espoo.vekkuli.config.MessageUtil
 import fi.espoo.vekkuli.domain.*
+import fi.espoo.vekkuli.utils.cmToM
 import fi.espoo.vekkuli.utils.mToCm
 import jakarta.servlet.http.HttpServletRequest
 import org.jdbi.v3.core.Jdbi
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
 import java.util.*
 
 @Controller
@@ -18,6 +23,9 @@ import java.util.*
 class CitizenUserController {
     @Autowired
     lateinit var jdbi: Jdbi
+
+    @Autowired
+    lateinit var messageUtil: MessageUtil
 
     @GetMapping("/kayttaja/{citizenId}")
     fun boatSpaceSearchPage(
@@ -44,7 +52,7 @@ class CitizenUserController {
     ): String {
         val boats = getBoatsForCitizen(citizenId, jdbi)
         val boat = boats.find { it.id == boatId } ?: throw IllegalArgumentException("Boat not found")
-        model.addAttribute("boat", boat)
+        model.addAttribute("boat", toUpdateForm(boat))
         model.addAttribute(
             "boatTypes",
             BoatType.entries.map { it.toString() }
@@ -56,52 +64,101 @@ class CitizenUserController {
         return "employee/edit-boat"
     }
 
-    data class BoatUpdateInput(
+    fun toUpdateForm(boat: Boat): BoatUpdateForm {
+        return BoatUpdateForm(
+            id = boat.id,
+            name = boat.name ?: "",
+            type = boat.type,
+            width = boat.widthCm.cmToM(),
+            length = boat.lengthCm.cmToM(),
+            depth = boat.depthCm.cmToM(),
+            weight = boat.weightKg,
+            registrationNumber = boat.registrationCode ?: "",
+            otherIdentifier = boat.otherIdentification ?: "",
+            extraInformation = boat.extraInformation ?: "",
+            ownership = boat.ownership
+        )
+    }
+
+    data class BoatUpdateForm(
+        val id: Int,
         val name: String,
-        val boatType: BoatType,
-        val width: Double,
-        val length: Double,
-        val depth: Double,
-        val weight: Int,
+        val type: BoatType,
+        val width: Double?,
+        val length: Double?,
+        val depth: Double?,
+        val weight: Int?,
         val registrationNumber: String,
         val otherIdentifier: String,
         val extraInformation: String,
         val ownership: OwnershipStatus,
     )
 
+    fun validateBoatUpdateInput(input: BoatUpdateForm): MutableMap<String, String> {
+        val errors = mutableMapOf<String, String>()
+        if (input.width == null) {
+            errors["width"] = messageUtil.getMessage("validation.required")
+        }
+        if (input.length == null) {
+            errors["length"] = messageUtil.getMessage("validation.required")
+        }
+        if (input.depth == null) {
+            errors["depth"] = messageUtil.getMessage("validation.required")
+        }
+        if (input.weight == null) {
+            errors["weight"] = messageUtil.getMessage("validation.required")
+        }
+        return errors
+    }
+
     @PatchMapping("/kayttaja/{citizenId}/vene/{boatId}")
     fun updateBoatPatch(
         request: HttpServletRequest,
         @PathVariable citizenId: UUID,
         @PathVariable boatId: Int,
-        input: BoatUpdateInput,
+        input: BoatUpdateForm,
         model: Model
-    ): String {
+    ): Any {
         val boats = getBoatsForCitizen(citizenId, jdbi)
         model.addAttribute("boats", boats)
         val boat = boats.find { it.id == boatId } ?: throw IllegalArgumentException("Boat not found")
-        model.addAttribute("boat", boat)
 
         val citizen = Citizen.getById(citizenId, jdbi)
         model.addAttribute("citizen", citizen)
         val boatSpaceReservations = BoatSpaceReservation.getReservationsForCitizen(citizenId, jdbi)
         model.addAttribute("reservations", boatSpaceReservations)
 
+        val errors = validateBoatUpdateInput(input)
+        model.addAttribute("errors", errors)
+
+        if (errors.isNotEmpty()) {
+            println("errors found")
+            model.addAttribute(
+                "boatTypes",
+                BoatType.entries.map { it.toString() }
+            )
+            model.addAttribute("ownershipOptions", listOf("Owner", "User", "CoOwner", "FutureOwner"))
+            model.addAttribute("boat", input)
+            return ModelAndView("employee/edit-boat", model.asMap(), HttpStatus.OK)
+        }
+
         val updatedBoat =
             boat.copy(
                 name = input.name,
-                type = input.boatType,
-                widthCm = input.width.mToCm(),
-                lengthCm = input.length.mToCm(),
-                depthCm = input.depth.mToCm(),
-                weightKg = input.weight,
+                type = input.type,
+                widthCm = input.width!!.mToCm(),
+                lengthCm = input.length!!.mToCm(),
+                depthCm = input.depth!!.mToCm(),
+                weightKg = input.weight!!,
                 registrationCode = input.registrationNumber,
                 otherIdentification = input.otherIdentifier,
                 extraInformation = input.extraInformation,
                 ownership = input.ownership
             )
         updateBoat(updatedBoat, jdbi)
+        val headers = org.springframework.http.HttpHeaders()
+        headers.add("HX-Redirect", "/virkailija/kayttaja/$citizenId")
 
-        return "employee/citizen-details"
+        return ResponseEntity<Any>(headers, HttpStatus.OK)
     }
 }
