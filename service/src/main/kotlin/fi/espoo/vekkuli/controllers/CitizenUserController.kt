ac9365a2d02c9035c7e2b6b66c326fdc6387831d
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-08-22 10:49:48 +0300

    Add inputs for all the boat fields

--- service/src/main/kotlin/fi/espoo/vekkuli/controllers/CitizenUserController.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/controllers/CitizenUserController.kt
@@ -1,10 +1,8 @@
 package fi.espoo.vekkuli.controllers
 
 import fi.espoo.vekkuli.config.MessageUtil
-import fi.espoo.vekkuli.domain.BoatSpaceReservation
-import fi.espoo.vekkuli.domain.Citizen
-import fi.espoo.vekkuli.domain.getBoatsForCitizen
-import fi.espoo.vekkuli.domain.updateBoat
+import fi.espoo.vekkuli.domain.*
+import fi.espoo.vekkuli.utils.mToCm
 import jakarta.servlet.http.HttpServletRequest
 import org.jdbi.v3.core.Jdbi
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.servlet.ModelAndView
 import java.util.*
 
 @Controller
@@ -55,19 +52,29 @@ class CitizenUserController {
         val boats = getBoatsForCitizen(citizenId, jdbi)
         val boat = boats.find { it.id == boatId } ?: throw IllegalArgumentException("Boat not found")
         model.addAttribute("boat", boat)
+        model.addAttribute(
+            "boatTypes",
+            BoatType.entries.map { it.toString() }
+        )
+
+        model.addAttribute("ownershipOptions", listOf("Owner", "User", "CoOwner", "FutureOwner"))
+
         model.addAttribute("errors", mutableMapOf<String, String>())
         return "employee/edit-boat"
     }
 
-    data class BoatUpdateInput(val name: String)
-
-    fun validateBoatUpdateInput(input: BoatUpdateInput): MutableMap<String, String> {
-        val errors = mutableMapOf<String, String>()
-        if (input.name.isBlank()) {
-            errors["name"] = messageUtil.getMessage("validation.required")
-        }
-        return errors
-    }
+    data class BoatUpdateInput(
+        val name: String,
+        val boatType: BoatType,
+        val width: Double,
+        val length: Double,
+        val depth: Double,
+        val weight: Int,
+        val registrationNumber: String,
+        val otherIdentifier: String,
+        val extraInformation: String,
+        val ownership: OwnershipStatus,
+    )
 
     @PatchMapping("/kayttaja/{citizenId}/vene/{boatId}")
     fun updateBoatPatch(
@@ -80,13 +87,20 @@ class CitizenUserController {
         val boats = getBoatsForCitizen(citizenId, jdbi)
         val boat = boats.find { it.id == boatId } ?: throw IllegalArgumentException("Boat not found")
         model.addAttribute("boat", boat)
-        val errors = validateBoatUpdateInput(input)
-        if (errors.isNotEmpty()) {
-            model.addAttribute("errors", errors)
-            return ModelAndView("employee/edit-boat", model.asMap(), HttpStatus.OK)
-        }
 
-        val updatedBoat = boat.copy(name = input.name)
+        val updatedBoat =
+            boat.copy(
+                name = input.name,
+                type = input.boatType,
+                widthCm = input.width.mToCm(),
+                lengthCm = input.length.mToCm(),
+                depthCm = input.depth.mToCm(),
+                weightKg = input.weight,
+                registrationCode = input.registrationNumber,
+                otherIdentification = input.otherIdentifier,
+                extraInformation = input.extraInformation,
+                ownership = input.ownership
+            )
         updateBoat(updatedBoat, jdbi)
 
         val headers = HttpHeaders()
