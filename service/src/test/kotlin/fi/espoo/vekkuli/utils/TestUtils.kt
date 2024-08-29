3040f8b22c7ad76f30071e6c5ff47794d287de81
merisaar <merisrnn@gmail.com>
2024-08-29 14:21:33 +0300

    move db seed function to common utils

--- service/src/test/kotlin/fi/espoo/vekkuli/utils/TestUtils.kt
+++ service/src/test/kotlin/fi/espoo/vekkuli/utils/TestUtils.kt
@@ -1,8 +1,14 @@
-package fi.espoo.vekkuli
+// SPDX-FileCopyrightText: 2023-2024 City of Espoo
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+package fi.espoo.vekkuli.utils
 
 import org.jdbi.v3.core.Jdbi
 import org.jdbi.v3.core.kotlin.withHandleUnchecked
 import java.io.File
+
+fun dataQa(s: String) = "[data-qa='$s']"
 
 fun createAndSeedDatabase(jdbi: Jdbi) {
     jdbi.withHandleUnchecked { tx ->
