19f74e88a5645cf8570e27031b3c61159ba5e153
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-09-03 07:56:49 +0300

    Fix registration number (#200)

--- service/src/main/kotlin/fi/espoo/vekkuli/views/citizen/BoatSpaceForm.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/views/citizen/BoatSpaceForm.kt
@@ -178,7 +178,7 @@ class BoatSpaceForm {
         val registrationNumberInput =
             formComponents.textInput(
                 "boatSpaceReservation.title.registrationNumber",
-                "registrationNumber",
+                "boatRegistrationNumber",
                 input.boatRegistrationNumber,
                 required = true
             )
@@ -188,11 +188,7 @@ class BoatSpaceForm {
                 "boatSpaceReservation.title.otherIdentifier",
                 "otherIdentification",
                 input.otherIdentification,
-                attributes =
-                    """
-                    :data-required="noReg ? true : null"
-                    """.trimIndent(),
-                labelAttributes = """:class="noReg ? 'required' : '' """"
+                required = true
             )
 
         val extraInformationInput =
@@ -316,8 +312,8 @@ class BoatSpaceForm {
                                         <input type="checkbox" 
                                                 name="noRegistrationNumber" 
                                                 id="noRegistrationNumber" 
-                                                @click="noReg = ! noReg"
-                                                ${if (input.noRegistrationNumber == null) "checked" else ""}
+                                                @click="noReg = !noReg"
+                                                ${if (input.noRegistrationNumber == true) "checked" else ""}
                                                 />
                                         <span>${t("boatApplication.noRegistrationNumber")}</span>
                                    </label> 
