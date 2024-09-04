a0596c2ae42d26b2b0d29bf882345cb995cd120c
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-09-04 14:21:46 +0300

    Convert reservation list and employee layout into kotlin functions

---
+++ service/src/main/kotlin/fi/espoo/vekkuli/views/Head.kt
@@ -0,0 +1,16 @@
+package fi.espoo.vekkuli.views
+
+// language=HTML
+val head =
+    """
+    <script src="https://unpkg.com/htmx.org@2.0.1"
+            integrity="sha384-QWGpdj554B4ETpJJC9z+ZHJcA/i59TyjxEPXiiUgN2WmTyV5OEZWCD6gQhgkdpB/"
+            crossorigin="anonymous"></script>
+
+    <meta name="htmx-config" content='{"allowNestedOobSwaps":false}'>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="/static/validation.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="/static/css/main.css">
+    ""${'"'}.trimIndent()
+    """.trimIndent()
