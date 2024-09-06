1c73b1ee9baf8657ea23d2545d6550314b4e1e48
Jukka Aittokallio <jai@reaktor.fi>
2024-09-06 09:39:57 +0300

    add table and repository functions for citizen memos (#219)

---
+++ service/src/main/kotlin/fi/espoo/vekkuli/domain/CitizenMemo.kt
@@ -0,0 +1,21 @@
+package fi.espoo.vekkuli.domain
+
+import java.time.LocalDateTime
+import java.util.*
+
+enum class MemoCategory {
+    Marine,
+    Spaces,
+    GroupExercise,
+}
+
+data class CitizenMemo(
+    val id: Int,
+    val createdAt: LocalDateTime,
+    val createdBy: UUID,
+    val updatedAt: LocalDateTime,
+    val updatedBy: UUID,
+    val category: MemoCategory,
+    val citizenId: UUID,
+    val content: String,
+)
