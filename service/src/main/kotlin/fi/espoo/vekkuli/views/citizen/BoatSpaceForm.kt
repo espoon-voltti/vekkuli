40a47d357bcbff26544c64651a298ab64a2f69f0
Jukka Aittokallio <jai@reaktor.fi>
2024-09-20 11:07:15 +0300

    don't show boat list to employee (#256)

--- service/src/main/kotlin/fi/espoo/vekkuli/views/citizen/BoatSpaceForm.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/views/citizen/BoatSpaceForm.kt
@@ -76,7 +76,8 @@ class BoatSpaceForm {
 
         // language=HTML
         val boatRadioButtons =
-            """
+            if (citizen !== null) {
+                """
             <div class="field" ">
                 <div class="radio">
                     <input type="radio" 
@@ -93,7 +94,12 @@ class BoatSpaceForm {
                 </div>
                 ${boats.joinToString("\n") { boatRadioButton(it) }}
             </div>
-            """.trimIndent()
+            """
+            } else {
+                """
+            <input type='hidden' name='boatId' value='0'>
+        """
+            }
 
         val boatTypeSelect =
             formComponents.select(
