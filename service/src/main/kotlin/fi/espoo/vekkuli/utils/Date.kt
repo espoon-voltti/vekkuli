298ef16de9b7872167e089c8db0c9d94d40448f5
Jukka Aittokallio <jai@reaktor.fi>
2024-09-23 08:39:58 +0300

    add domain logic for reservation periods (#261)
    * add domain logic for reservation periods

---
+++ service/src/main/kotlin/fi/espoo/vekkuli/utils/Date.kt
@@ -0,0 +1,32 @@
+package fi.espoo.vekkuli.utils
+
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+
+fun getFirstWeekdayOfMonth(
+    year: Int,
+    month: Int
+): LocalDate {
+    val firstDay = LocalDate.of(year, month, 1)
+    return when (firstDay.dayOfWeek) {
+        DayOfWeek.SATURDAY -> firstDay.plusDays(2)
+        DayOfWeek.SUNDAY -> firstDay.plusDays(1)
+        else -> firstDay
+    }
+}
+
+fun getLastDayOfYear(year: Int): LocalDate = LocalDate.of(year, 12, 31)
+
+fun getLastDayOfNextYearsJanuary(year: Int): LocalDate = LocalDate.of(year + 1, 1, 31)
+
+fun isTimeWithinDateRange(
+    dateTime: LocalDateTime,
+    startDate: LocalDate,
+    endDate: LocalDate
+): Boolean {
+    val startDateTime = startDate.atStartOfDay() // Start of the day (00:00:00)
+    val endDateTime = endDate.atTime(LocalTime.MAX) // End of the day (23:59:999)
+    return !dateTime.isBefore(startDateTime) && !dateTime.isAfter(endDateTime)
+}
