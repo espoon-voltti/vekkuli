d1ebe73697ccc72a70ee906181a87be667c50586
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-10-10 13:07:36 +0300

    Fix favicon location

--- service/src/main/kotlin/fi/espoo/vekkuli/views/Head.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/views/Head.kt
@@ -12,4 +12,5 @@ val head =
     <script src="/static/validation.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="/static/css/main.css">
+    <link rel="icon" type="image/x-icon" href="/static/favicon.ico">
     """.trimIndent()
