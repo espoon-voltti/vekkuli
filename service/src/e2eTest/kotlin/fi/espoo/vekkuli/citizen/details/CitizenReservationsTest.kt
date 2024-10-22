f97eb6a92ed30b4a0479f16c1ba516de3d439ffa
Mauno Lempiäinen <mauno.lempiainen@reaktor.com>
2024-10-22 14:29:48 +0300

    Add 2 expired and 1 unconfirmed reservations for seed test file and e2e test for boat reservations lists

---
+++ service/src/e2eTest/kotlin/fi/espoo/vekkuli/citizen/details/CitizenReservationsTest.kt
@@ -0,0 +1,55 @@
+package fi.espoo.vekkuli.citizen.details
+
+import com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat
+import fi.espoo.vekkuli.PlaywrightTest
+import fi.espoo.vekkuli.baseUrl
+import fi.espoo.vekkuli.pages.CitizenDetailsPage
+import org.junit.jupiter.api.Test
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+class CitizenReservationsTest : PlaywrightTest() {
+    @Test
+    fun `citizen can see their active reservations`() {
+        try {
+            val citizenDetailsPage = CitizenDetailsPage(page)
+
+            page.navigate(baseUrl)
+            page.getByTestId("loginButton").click()
+            page.getByTestId("031298-988S").click() // Olivia Virtanen
+            page.getByText("Kirjaudu").click()
+            page.navigate(baseUrl + "/kuntalainen/omat-tiedot")
+
+            // Opens up information from the first reservation of the first user
+            assertThat(citizenDetailsPage.locationNameInFirstBoatSpaceReservationCard).hasText("Haukilahti")
+            assertThat(citizenDetailsPage.placeInFirstBoatSpaceReservationCard).hasText("B3")
+
+            // Seed user has 1 active reservations
+            assertThat(citizenDetailsPage.reservationListCards).hasCount(1)
+        } catch (e: AssertionError) {
+            handleError(e)
+        }
+    }
+
+    @Test
+    fun `citizen can see their expired reservations`() {
+        try {
+            val citizenDetailsPage = CitizenDetailsPage(page)
+
+            page.navigate(baseUrl)
+            page.getByTestId("loginButton").click()
+            page.getByTestId("031298-988S").click() // Olivia Virtanen
+            page.getByText("Kirjaudu").click()
+            page.navigate(baseUrl + "/kuntalainen/omat-tiedot")
+
+            // Opens up information from the first reservation of the first user
+            assertThat(citizenDetailsPage.locationNameInFirstBoatSpaceReservationCard).hasText("Haukilahti")
+            assertThat(citizenDetailsPage.placeInFirstBoatSpaceReservationCard).hasText("B3")
+
+            // Seed user has 2 expired reservations
+            assertThat(citizenDetailsPage.expiredReservationListCards).hasCount(2)
+        } catch (e: AssertionError) {
+            handleError(e)
+        }
+    }
+}
