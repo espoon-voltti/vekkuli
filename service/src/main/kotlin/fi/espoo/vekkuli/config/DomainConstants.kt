3729ff6df6fdf3a06dd87f82872ac7cb0b9d639f
merisaar <merisrnn@gmail.com>
2024-10-31 11:05:29 +0200

    add suggested changes

--- service/src/main/kotlin/fi/espoo/vekkuli/config/DomainConstants.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/config/DomainConstants.kt
@@ -3,4 +3,5 @@ package fi.espoo.vekkuli.config
 object DomainConstants {
     const val ESPOO_MUNICIPALITY_CODE = 49
     const val FAILED_MESSAGE_RETRY_LIMIT = 5
+    const val INVOICE_PAYMENT_PERIOD = 14
 }
