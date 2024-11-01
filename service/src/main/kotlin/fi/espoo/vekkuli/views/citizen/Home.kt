76a28a3b0cec6ea0d75c575484b1740c3ed32edc
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-11-01 15:01:33 +0200

    Display current date in the Employee home page

--- service/src/main/kotlin/fi/espoo/vekkuli/views/citizen/Home.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/views/citizen/Home.kt
@@ -1,37 +1,27 @@
 package fi.espoo.vekkuli.views.citizen
 
 import fi.espoo.vekkuli.config.MessageUtil
-import fi.espoo.vekkuli.controllers.EnvType
-import fi.espoo.vekkuli.controllers.Utils.Companion.getEnv
 import fi.espoo.vekkuli.service.MarkDownService
-import fi.espoo.vekkuli.utils.TimeProvider
+import fi.espoo.vekkuli.views.common.CurrentDate
 import org.springframework.stereotype.Service
 
 @Service
 class Home(
     private val messageUtil: MessageUtil,
     private val markDownService: MarkDownService,
-    private val timeProvider: TimeProvider
+    private val currentDate: CurrentDate
 ) {
     fun t(key: String): String = messageUtil.getMessage(key)
 
     fun render(): String {
         // language=HTML
-
-        val date =
-            if (getEnv() != EnvType.Production) {
-                "<div>Päivämäärä: ${timeProvider.getCurrentDate()}</div>"
-            } else {
-                ""
-            }
-
         return """
             <section class="section">
                 <div class="container">
                     <h2>${t("boatSpaces.title")}</h2>
                     ${markDownService.render(t("frontPage.content"))}
+                    ${currentDate.render()}
                     <div class="block"><a class="button is-primary" href="/kuntalainen/venepaikat">Varaa venepaikka</button></a>
-                    $date
                 </div>
             </section>
             """.trimIndent()
