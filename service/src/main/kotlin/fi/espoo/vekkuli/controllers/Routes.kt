c23b7e05bc26ec77bd84e455133c57cb4dbe5fbe
merisaar <merisrnn@gmail.com>
2024-11-06 15:26:12 +0200

    wip

--- service/src/main/kotlin/fi/espoo/vekkuli/controllers/Routes.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/controllers/Routes.kt
@@ -1,14 +1,15 @@
 package fi.espoo.vekkuli.controllers
 
-enum class UserType(val path: String) {
+enum class UserType(
+    val path: String
+) {
     CITIZEN("kuntalainen"),
     EMPLOYEE("virkailija");
 
     companion object {
-        fun fromPath(path: String): UserType {
-            return entries.find { it.path == path }
+        fun fromPath(path: String): UserType =
+            entries.find { it.path == path }
                 ?: throw IllegalArgumentException("Invalid user type")
-        }
     }
 }
 
