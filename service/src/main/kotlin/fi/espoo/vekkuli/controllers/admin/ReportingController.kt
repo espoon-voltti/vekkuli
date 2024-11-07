ef71fa1db4d249d2fae09efec61a80b73b28f400
Joonas Reynders <joonas.reynders@reaktor.fi>
2024-11-07 07:51:33 +0200

    Reports view and raw report link

--- service/src/main/kotlin/fi/espoo/vekkuli/controllers/admin/ReportingController.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/controllers/admin/ReportingController.kt
@@ -1,30 +1,49 @@
-package fi.espoo.vekkuli.controllers
+package fi.espoo.vekkuli.controllers.admin
 
 import fi.espoo.vekkuli.common.Forbidden
 import fi.espoo.vekkuli.config.audit
 import fi.espoo.vekkuli.config.getAuthenticatedUser
 import fi.espoo.vekkuli.service.getRawReport
 import fi.espoo.vekkuli.service.rawReportToCsv
+import fi.espoo.vekkuli.views.admin.Layout
+import fi.espoo.vekkuli.views.admin.ReportingView
 import jakarta.servlet.http.HttpServletRequest
 import mu.KotlinLogging
 import org.jdbi.v3.core.Jdbi
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
-@Controller
-class ReportingController {
+@RestController
+@RequestMapping("/admin/reporting")
+class ReportingController(
+    private val reportingLayout: Layout,
+    private val reportingView: ReportingView
+) {
     @Autowired
     lateinit var jdbi: Jdbi
 
     private val logger = KotlinLogging.logger {}
 
-    @GetMapping("/reporting/raw-report", produces = ["text/csv"])
+    @GetMapping("/raw-report", produces = ["text/csv"])
     @ResponseBody
     fun rawReport(request: HttpServletRequest): ResponseEntity<String> {
         val authenticatedUser = request.getAuthenticatedUser() ?: throw Forbidden("No authenticated user")
         logger.audit(authenticatedUser, "DOWNLOAD_RAW_REPORT")
         return ResponseEntity.ok(rawReportToCsv(getRawReport(jdbi)))
     }
+
+    @GetMapping
+    @ResponseBody
+    fun getReportingView(request: HttpServletRequest) =
+        ResponseEntity
+            .ok()
+            .header("Content-Type", "text/html")
+            .body(
+                reportingLayout.render(
+                    reportingLayout.render(
+                        reportingView.render()
+                    )
+                )
+            )
 }
