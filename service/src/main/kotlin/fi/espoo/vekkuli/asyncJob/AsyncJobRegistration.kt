dff6c4cf7631f1a2a773fb4173991e263fd066bd
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-11-14 14:50:07 +0200

    Register handler on ContextRefreshedEvent

--- service/src/main/kotlin/fi/espoo/vekkuli/asyncJob/AsyncJobRegistration.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/asyncJob/AsyncJobRegistration.kt
@@ -1,14 +1,18 @@
 package fi.espoo.vekkuli.asyncJob
 
 import fi.espoo.vekkuli.service.InvoiceClient
+import org.springframework.context.ApplicationListener
+import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.stereotype.Service
 
 @Service
 class AsyncJobRegistration(
-    asyncJobRunner: AsyncJobRunner<AsyncJob>,
-    invoiceClient: InvoiceClient
-) {
-    init {
-        asyncJobRunner.registerHandler<AsyncJob.SendInvoiceBatch> { job -> invoiceClient.sendBatchInvoice(job.invoiceBatch) }
+    private val asyncJobRunner: AsyncJobRunner<AsyncJob>,
+    private val invoiceClient: InvoiceClient
+) : ApplicationListener<ContextRefreshedEvent> {
+    override fun onApplicationEvent(event: ContextRefreshedEvent) {
+        asyncJobRunner.registerHandler<AsyncJob.SendInvoiceBatch> { job ->
+            invoiceClient.sendBatchInvoice(job.invoiceBatch)
+        }
     }
 }
