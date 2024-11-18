c3f9cbf2ad8f4a449e1f52c6927785c9e56116fc
Mauno Lempiäinen <mauno.lempiainen@reaktor.com>
2024-11-18 12:29:43 +0200

    Add missing controller auth for admin views and fix patch conflict

---
+++ service/src/main/kotlin/fi/espoo/vekkuli/boatSpace/admin/AuthControllerAdvice.kt
@@ -0,0 +1,25 @@
+package fi.espoo.vekkuli.boatSpace.admin
+
+import fi.espoo.vekkuli.common.NotFound
+import fi.espoo.vekkuli.common.Unauthorized
+import fi.espoo.vekkuli.config.getAuthenticatedUser
+import fi.espoo.vekkuli.controllers.EnvType
+import fi.espoo.vekkuli.controllers.Utils.Companion.getEnv
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@ControllerAdvice(basePackages = ["fi.espoo.vekkuli.boatSpace.admin"])
+class AuthControllerAdvice : ResponseEntityExceptionHandler() {
+    @ModelAttribute
+    fun handleAuth(request: HttpServletRequest) {
+        val isEmployee = request.getAuthenticatedUser()?.isEmployee() ?: false
+        if ((getEnv() != EnvType.Staging && getEnv() != EnvType.Local) || isEmployee) {
+            throw NotFound()
+        }
+        if (request.getAuthenticatedUser()?.id == null) {
+            throw Unauthorized()
+        }
+    }
+}
