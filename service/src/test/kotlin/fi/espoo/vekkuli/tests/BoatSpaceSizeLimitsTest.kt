beba03998ad822dce3e4b5ff0b51d1d6ca30fab1
Jukka Aittokallio <jai@reaktor.fi>
2024-11-21 10:52:57 +0200

    fix test

--- service/src/test/kotlin/fi/espoo/vekkuli/tests/BoatSpaceSizeLimitsTest.kt
+++ service/src/test/kotlin/fi/espoo/vekkuli/tests/BoatSpaceSizeLimitsTest.kt
@@ -41,7 +41,7 @@ class BoatSpaceSizeLimitsTest {
     @CsvSource(
         "Buoy, 200, 300, 0, 2147483647, 0, 2147483647",
         "RearBuoy, 200, 600, 250, 2147483647, 900, 2147483647",
-        "Beam, 200, 200, 240, 300, 70, 2147483647",
+        "Beam, 200, 200, 240, 2147483647, 70, 2147483647",
         "WalkBeam, 200, 300, 275, 300, 170, 450"
     )
     fun `given boat size, should get limits for boat space size`(
