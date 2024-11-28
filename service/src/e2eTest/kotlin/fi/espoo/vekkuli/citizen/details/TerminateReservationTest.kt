a0e09ffabcdb585ef71cc24cbba6db9a940266a3
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-11-28 15:25:08 +0200

    Revert "Disable submit buttons when a request is in flight (#465)"
    This reverts commit fa7a71bcc1a0b4c7c64ad1882c25a26019c5b75d.

--- service/src/e2eTest/kotlin/fi/espoo/vekkuli/citizen/details/TerminateReservationTest.kt
+++ service/src/e2eTest/kotlin/fi/espoo/vekkuli/citizen/details/TerminateReservationTest.kt
@@ -78,7 +78,6 @@ class TerminateReservationTest : PlaywrightTest() {
             assertThat(citizenDetailsPage.terminateReservationFormLocation).hasText("Haukilahti B 001")
 
             citizenDetailsPage.terminateReservationModalConfirm.click()
-            assertThat(citizenDetailsPage.terminateReservationModalConfirm).isDisabled()
 
             // Shows a success message in modal
             assertThat(citizenDetailsPage.terminateReservationSuccess).isVisible()
