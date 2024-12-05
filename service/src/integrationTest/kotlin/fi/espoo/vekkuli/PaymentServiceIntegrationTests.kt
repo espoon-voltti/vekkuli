2aa8634d14cfb266cc73bb06e760efee8c62b142
Mauno Lempiäinen <mauno.lempiainen@reaktor.com>
2024-12-05 15:33:52 +0200

    Fix import order

--- service/src/integrationTest/kotlin/fi/espoo/vekkuli/PaymentServiceIntegrationTests.kt
+++ service/src/integrationTest/kotlin/fi/espoo/vekkuli/PaymentServiceIntegrationTests.kt
@@ -1,7 +1,7 @@
 package fi.espoo.vekkuli
 
-import fi.espoo.vekkuli.boatSpace.reservationStatus.ReservationStatus
 import fi.espoo.vekkuli.boatSpace.invoice.BoatSpaceInvoiceService
+import fi.espoo.vekkuli.boatSpace.reservationStatus.ReservationStatus
 import fi.espoo.vekkuli.domain.*
 import fi.espoo.vekkuli.service.*
 import org.junit.jupiter.api.Assertions.assertEquals
