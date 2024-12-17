46ac776f0591a0146e92cb67929f71b643175b63
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-12-17 10:13:40 +0200

    Better comment for the migration

--- service/src/main/resources/db/migration/V052__add_reserver_id_to_payment.sql
+++ service/src/main/resources/db/migration/V052__add_reserver_id_to_payment.sql
@@ -1,3 +1,5 @@
+-- Replace the citizen_id column with the reserver_id column in the payment and invoice tables.
+
 ALTER TABLE payment ADD COLUMN reserver_id uuid NOT NULL DEFAULT uuid_nil();
 
 UPDATE payment SET reserver_id = citizen_id;
@@ -15,24 +17,17 @@ DROP INDEX idx_payment_citizen_id;
 
 ALTER TABLE payment DROP COLUMN citizen_id;
 
--- First add the new column
 ALTER TABLE invoice ADD COLUMN reserver_id uuid NOT NULL DEFAULT uuid_nil();
 
--- Copy the data from citizen_id to reserver_id
 UPDATE invoice SET reserver_id = citizen_id;
 
--- Remove the DEFAULT after data is copied
 ALTER TABLE invoice ALTER COLUMN reserver_id DROP DEFAULT;
 
--- Add foreign key constraint to reference the reserver table
 ALTER TABLE invoice ADD CONSTRAINT invoice_reserver_id_fkey
     FOREIGN KEY (reserver_id) REFERENCES reserver(id);
 
--- Add index for the new column
 CREATE INDEX idx_invoice_reserver_id ON invoice(reserver_id);
 
--- Drop the foreign key constraint for the old column
 ALTER TABLE invoice DROP CONSTRAINT invoice_citizen_id_fkey;
 
--- Finally drop the citizen_id column
 ALTER TABLE invoice DROP COLUMN citizen_id;
