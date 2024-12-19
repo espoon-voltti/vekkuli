640ae91dfa7a720b79dba4ad861f3269fc7b4aa5
Jukka Aittokallio <jai@reaktor.fi>
2024-12-19 16:18:28 +0200

    add reservation clearing tool to admin page (#554)

---
+++ service/src/main/kotlin/fi/espoo/vekkuli/boatSpace/admin/reservation/ReservationController.kt
@@ -0,0 +1,105 @@
+package fi.espoo.vekkuli.boatSpace.admin.reservation
+
+import fi.espoo.vekkuli.controllers.EnvType
+import fi.espoo.vekkuli.controllers.Utils.Companion.getEnv
+import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.kotlin.mapTo
+import org.jdbi.v3.core.kotlin.withHandleUnchecked
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import java.util.UUID
+
+@RestController
+@RequestMapping("/admin")
+class ReservationController {
+    @Autowired
+    lateinit var reservationView: ReservationView
+
+    @Autowired
+    lateinit var jdbi: Jdbi
+
+    @PostMapping("/reservations")
+    fun clearReservations(
+        @RequestParam reserverName: String,
+        @RequestParam user: UserColumn,
+    ): String {
+        if (getEnv() !in setOf(EnvType.Staging, EnvType.Local)) {
+            return ""
+        }
+
+        val query =
+            when (user) {
+                UserColumn.Reserver -> """SELECT id FROM reserver WHERE (name ILIKE '%$reserverName%')"""
+                UserColumn.ActingUser -> """SELECT id FROM reserver WHERE (name ILIKE '%$reserverName%')"""
+                UserColumn.Employee -> """SELECT id FROM app_user WHERE (CONCAT(last_name, ' ', first_name) ILIKE '%$reserverName%')"""
+            }
+
+        val reservers =
+            jdbi.withHandleUnchecked { handle ->
+                handle
+                    .createQuery(query)
+                    .mapTo<UUID>()
+                    .list()
+            }
+
+        return when (reservers.size) {
+            0 -> reservationView.render("Varaajaa ei löytynyt")
+            1 -> reservationView.render(deleteReservations(reservers[0], user))
+            else ->
+                reservationView.render("Varaajia löytyi useita. Tarkenna nimeä.")
+        }
+    }
+
+    fun deleteReservations(
+        userId: UUID,
+        userColumn: UserColumn
+    ): String {
+        val query = "SELECT id FROM boat_space_reservation WHERE ${userColumn.toName()} = :userId"
+        return jdbi.withHandleUnchecked { handle ->
+            // Read all reservation ids for the reserver
+            val reservationIds =
+                handle
+                    .createQuery(
+                        query,
+                    ).bind("userId", userId)
+                    .mapTo<Int>()
+                    .list()
+
+            if (reservationIds.isEmpty()) return@withHandleUnchecked "Ei löytynyt varauksia henkilölle $userId"
+
+            val ids = reservationIds.joinToString(", ")
+
+            // Delete invoices
+            handle
+                .createUpdate(
+                    """    
+                        DELETE FROM invoice WHERE reservation_id IN ($ids)
+                    """
+                ).execute()
+
+            // Delete payments
+            handle
+                .createUpdate(
+                    """    
+                        DELETE FROM payment WHERE reservation_id IN ($ids)
+                    """
+                ).execute()
+
+            // Delete warnings
+            handle
+                .createUpdate(
+                    """    
+                        DELETE FROM reservation_warning WHERE reservation_id IN ($ids)
+                    """
+                ).execute()
+
+            // Delete reservations
+            handle
+                .createUpdate(
+                    "DELETE FROM boat_space_reservation WHERE id IN ($ids)",
+                ).execute()
+
+            "Henkilöltä $userId poistettu seuraavat varaukset: $ids"
+        }
+    }
+}
---
+++ service/src/main/kotlin/fi/espoo/vekkuli/boatSpace/admin/reservation/ReservationView.kt
@@ -0,0 +1,63 @@
+package fi.espoo.vekkuli.boatSpace.admin.reservation
+
+import fi.espoo.vekkuli.FormComponents
+import fi.espoo.vekkuli.RadioOption
+import fi.espoo.vekkuli.config.MessageUtil
+import fi.espoo.vekkuli.controllers.EnvType
+import fi.espoo.vekkuli.controllers.Utils.Companion.getEnv
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+enum class UserColumn {
+    Reserver,
+    ActingUser,
+    Employee;
+
+    fun toName(): String =
+        when (this) {
+            Reserver -> "reserver_id"
+            ActingUser -> "acting_user_id"
+            Employee -> "employee_id"
+        }
+}
+
+@Service
+class ReservationView {
+    @Autowired
+    lateinit var components: FormComponents
+
+    @Autowired
+    lateinit var messageUtil: MessageUtil
+
+    fun render(resultMessage: String? = null): String {
+        if (getEnv() !in setOf(EnvType.Staging, EnvType.Local)) {
+            return ""
+        }
+        val radios =
+            components.radioButtons(
+                "admin.reservation.title",
+                "user",
+                UserColumn.Reserver.name,
+                UserColumn.entries.map { RadioOption(it.name, messageUtil.getMessage("admin.reservation.userOption.${it.name}")) }
+            )
+        return """
+            <section class="section" id="reservation-clear">
+                <div class="container">
+                   <h2>Varausten poisto</h2>
+                   <form hx-post="/admin/reservations"
+                         hx-target="#reservation-clear">
+                      <div class="field">
+                        <label class="label">Nimi</label>
+                        <div class="control">
+                            <input type="text" name="reserverName"/>
+                        </div>
+                      </div>
+                      $radios
+                      <button class="button is-primary">Poista varaukset</button>
+                      ${if (resultMessage.isNullOrBlank()) "" else """<p class="has-text-success"">$resultMessage</p>"""}
+                   </form>
+                </div>
+            </section>
+            """.trimIndent()
+    }
+}
