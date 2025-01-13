d29312bde1a6237f0470f67bd1a1931438c5848d
Ari Paartola <ari.paartola@reaktor.com>
2025-01-13 13:39:08 +0200

    Show discount options for reserver.

--- service/src/main/kotlin/fi/espoo/vekkuli/views/employee/components/ReserverDetailsExceptionsContainer.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/views/employee/components/ReserverDetailsExceptionsContainer.kt
@@ -14,8 +14,8 @@ class ReserverDetailsExceptionsContainer : BaseView() {
     private fun espooRulesAppliedContent(reserver: ReserverWithDetails): String {
         // language=HTML
         return """
-            <label class="checkbox">            
-                <input type="checkbox" id="edit-espoorules-applied-button"
+            <div class="espoo-rules-applied checkbox">            
+                <input type="checkbox" id="edit-espoorules-applied-checkbox"
                     ${if (reserver.espooRulesApplied) "checked" else "" }
                     hx-patch="${reserverDetailsTabs.getTabUrl("${reserver.id}/poikkeukset/toggle-espoo-rules-applied")}"
                     hx-trigger="click"
@@ -23,7 +23,42 @@ class ReserverDetailsExceptionsContainer : BaseView() {
                     hx-swap="outerHTML"
                 >                
                 <span>${t("employee.reserverDetails.exceptions.espooExplanation")}</span>                
-            </label>
+            </div>
+            """.trimIndent()
+    }
+
+    private fun discountContent(reserver: ReserverWithDetails): String {
+        // language=HTML
+        return """
+            <div class="discounts">
+                <div class="radio">            
+                    <input type="radio" id="reserver_discount_0" name="reserverDiscount" value='0'
+                        ${if (reserver.discountPercentage == 0) "checked" else ""}
+                        hx-patch="${reserverDetailsTabs.getTabUrl("${reserver.id}/poikkeukset/discount")}"
+                        hx-trigger="click"
+                        hx-target="#tab-content"
+                        hx-swap="outerHTML">
+                        <label for="reserver_discount_0" >${t("employee.reserverDetails.exceptions.discount_0")}</label>
+                </div>
+                <div class="radio">
+                    <input type="radio" id="reserver_discount_50" name="reserverDiscount" value='50'
+                        ${if (reserver.discountPercentage == 50) "checked" else ""}
+                        hx-patch="${reserverDetailsTabs.getTabUrl("${reserver.id}/poikkeukset/discount")}"
+                        hx-trigger="click"
+                        hx-target="#tab-content"
+                        hx-swap="outerHTML">
+                        <label for="reserver_discount_50" >${t("employee.reserverDetails.exceptions.discount_50")}</label>
+                </div>
+                <div class="radio">
+                    <input type="radio" id="reserver_discount_100" name="reserverDiscount" value='100'
+                        ${if (reserver.discountPercentage == 100) "checked" else ""}
+                        hx-patch="${reserverDetailsTabs.getTabUrl("${reserver.id}/poikkeukset/discount")}"
+                        hx-trigger="click"
+                        hx-target="#tab-content"
+                        hx-swap="outerHTML">
+                        <label for="reserver_discount_100" >${t("employee.reserverDetails.exceptions.discount_100")}</label>
+                </div>
+                </div>
             """.trimIndent()
     }
 
@@ -35,6 +70,8 @@ class ReserverDetailsExceptionsContainer : BaseView() {
               <div class="exceptions-container">
                 <label class="label">${t("employee.reserverDetails.exceptions.espooTitle")}</label>
                 ${espooRulesAppliedContent(reserver)}
+                <label class="label">${t("employee.reserverDetails.exceptions.discountTitle")}</label>
+                ${discountContent(reserver)}
               </div>              
             </div>
             """.trimIndent()
