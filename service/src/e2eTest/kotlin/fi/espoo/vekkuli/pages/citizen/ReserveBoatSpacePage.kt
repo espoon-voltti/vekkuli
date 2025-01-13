b85b3522df048ebdf0dcbaed5b0ab575d1303154
Jari Helenius <jari.helenius@reaktor.com>
2025-01-13 14:59:48 +0200

    Fix linter errors

--- service/src/e2eTest/kotlin/fi/espoo/vekkuli/pages/citizen/ReserveBoatSpacePage.kt
+++ service/src/e2eTest/kotlin/fi/espoo/vekkuli/pages/citizen/ReserveBoatSpacePage.kt
@@ -31,26 +31,30 @@ class ReserveBoatSpacePage(
     }
 
     class ElementLocator(private val locator: Locator) {
-        fun getInput(label: String) = locator.locator("label")
-            .getByText(label)
-            .locator("..")
-            .locator("input")
+        fun getInput(label: String) =
+            locator.locator("label")
+                .getByText(label)
+                .locator("..")
+                .locator("input")
 
-        fun getCheckbox(label: String) = locator.locator("label.checkbox span")
-            .getByText(label, Locator.GetByTextOptions().setExact(true))
-            .locator("..")
-            .locator("input[type=checkbox]")
+        fun getCheckbox(label: String) =
+            locator.locator("label.checkbox span")
+                .getByText(label, Locator.GetByTextOptions().setExact(true))
+                .locator("..")
+                .locator("input[type=checkbox]")
 
-        fun getRadio(label: String) = locator.locator("label .body")
-            .getByText(label, Locator.GetByTextOptions().setExact(true))
-            .locator("..")
-            .locator("..")
-            .locator("input[type=radio]")
+        fun getRadio(label: String) =
+            locator.locator("label .body")
+                .getByText(label, Locator.GetByTextOptions().setExact(true))
+                .locator("..")
+                .locator("..")
+                .locator("input[type=radio]")
 
-        fun getSelect(label: String) = locator.locator("label")
-            .getByText(label, Locator.GetByTextOptions().setExact(true))
-            .locator("..")
-            .locator("select")
+        fun getSelect(label: String) =
+            locator.locator("label")
+                .getByText(label, Locator.GetByTextOptions().setExact(true))
+                .locator("..")
+                .locator("select")
     }
 
     fun navigateToPage() {
@@ -58,6 +62,6 @@ class ReserveBoatSpacePage(
     }
 
     fun getFilterSection() = FilterSection(ElementLocator(getByDataTestId("boat-space-filter")))
+
     fun getSearchResultsSection() = SearchResultsSection(getByDataTestId("boat-space-results"))
 }
-
