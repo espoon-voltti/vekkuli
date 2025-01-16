17124c0790b61c5cbae4ce99f13a7125b83dfab9
Ari Paartola <ari.paartola@reaktor.com>
2025-01-16 17:20:48 +0200

    Take only one unfinished reservation to prevent reservation list crash

--- service/src/main/kotlin/fi/espoo/vekkuli/repository/JdbiBoatSpaceReservationRepository.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/repository/JdbiBoatSpaceReservationRepository.kt
@@ -370,6 +370,7 @@ class JdbiBoatSpaceReservationRepository(
                     WHERE bsr.employee_id = :id
                         AND bsr.status IN ('Info', 'Payment') 
                         AND :currentTime BETWEEN bsr.created AND bsr.created + make_interval(secs => :sessionTimeInSeconds)
+                        ORDER BY bsr.created DESC LIMIT 1
                     """.trimIndent()
                 )
             query.bind("id", id)
