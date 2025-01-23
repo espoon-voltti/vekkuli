18d7d4533c3dd937f1d94516386ab5ac33ebd390
Väinö Jalonen <vaino.jalonen@reaktor.com>
2025-01-23 14:15:27 +0200

    Migration to fix reservation periods

---
+++ service/src/main/resources/db/migration/V061__fix_reservation_periods.sql
@@ -0,0 +1,32 @@
+DELETE FROM reservation_period;
+
+INSERT INTO reservation_period(is_espoo_citizen, boat_space_type, operation, start_month, start_day, end_month, end_day)
+VALUES
+    -- For Espoo citizens
+    (true, 'Slip', 'New', 3, 1, 9, 30),
+    (true, 'Slip', 'Renew', 1, 7, 1, 31),
+    (true, 'Slip', 'Change', 1, 7, 1, 31),
+    (true, 'Slip', 'Change', 3, 1, 9, 30),
+    (true, 'Trailer', 'New', 5, 1, 12, 31),
+    (true, 'Trailer', 'Renew', 4, 1, 4, 30),
+    (true, 'Trailer', 'Change',  4, 1, 12, 31),
+    (true, 'Winter', 'New', 9, 1, 12, 31),
+    (true, 'Winter', 'Renew',  8, 1, 8, 31),
+    (true, 'Winter', 'Change',  8, 1, 12, 31),
+    -- The following line was changed from previous migrations
+    (true, 'Storage', 'New', 9, 1, 7, 31),
+    (true, 'Storage', 'Renew',  8, 1, 8, 31),
+    (true, 'Storage', 'Change',  8, 1, 7, 31),
+
+    -- For non-Espoo citizens
+    (false, 'Slip', 'New', 4, 1, 9, 30),
+    (false, 'Slip', 'Change', 4, 1, 9, 30),
+    (false, 'Trailer', 'New', 5, 1, 12, 31),
+    (false, 'Trailer', 'Change',  5, 1, 12, 31),
+    (false, 'Winter', 'New', 9, 15, 12, 31),
+    (false, 'Winter', 'Change',  9, 15, 12, 31),
+    (false, 'Storage', 'New', 9, 15, 7, 31),
+    (false, 'Storage', 'Renew', 9, 1, 9, 15),
+    (false, 'Storage', 'Change',  9, 1, 7, 31),
+
+    (true, 'Slip', 'SecondNew', 4, 1, 9, 30);
