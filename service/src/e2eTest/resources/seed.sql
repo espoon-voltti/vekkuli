c9665e38bb907691476c82b1554c5b44bca17c76
Väinö Jalonen <vaino.jalonen@reaktor.com>
2025-01-27 10:54:54 +0200

    Add reservation periods to the seed, but clear the table before seeding

--- service/src/e2eTest/resources/seed.sql
+++ service/src/e2eTest/resources/seed.sql
@@ -482,6 +482,37 @@ INSERT INTO harbor_restriction (location_id, excluded_boat_type) VALUES (4, 'Sai
 INSERT INTO harbor_restriction (location_id, excluded_boat_type) VALUES (7, 'JetSki');
 INSERT INTO harbor_restriction (location_id, excluded_boat_type) VALUES (7, 'Sailboat');
 
+DELETE FROM reservation_period;
+
+INSERT INTO reservation_period(is_espoo_citizen, boat_space_type, operation, start_month, start_day, end_month, end_day)
+VALUES
+    -- For Espoo citizens
+    (true, 'Slip', 'New', 3, 1, 9, 30),
+    (true, 'Slip', 'SecondNew', 4, 1, 9, 30),
+    (true, 'Slip', 'Renew', 1, 7, 1, 31),
+    (true, 'Slip', 'Change', 1, 7, 1, 31),
+    (true, 'Slip', 'Change', 3, 1, 9, 30),
+    (true, 'Trailer', 'New', 5, 1, 12, 31),
+    (true, 'Trailer', 'Renew', 4, 1, 4, 30),
+    (true, 'Trailer', 'Change',  4, 1, 12, 31),
+    (true, 'Winter', 'New', 9, 1, 12, 31),
+    (true, 'Winter', 'Renew',  8, 1, 8, 31),
+    (true, 'Winter', 'Change',  8, 1, 12, 31),
+    (true, 'Winter', 'SecondNew',  9, 1, 12, 31),
+    (true, 'Storage', 'New', 9, 1, 7, 31),
+    (true, 'Storage', 'Renew',  8, 1, 8, 31),
+    (true, 'Storage', 'Change',  8, 1, 7, 31),
+
+    -- For non-Espoo citizens
+    (false, 'Slip', 'New', 4, 1, 9, 30),
+    (false, 'Slip', 'Change', 4, 1, 9, 30),
+    (false, 'Trailer', 'New', 5, 1, 12, 31),
+    (false, 'Trailer', 'Change',  5, 1, 12, 31),
+    (false, 'Storage', 'New', 9, 15, 7, 31),
+    (false, 'Storage', 'Renew', 9, 1, 9, 15),
+    (false, 'Storage', 'Change',  9, 1, 7, 31);
+
+
 INSERT INTO app_user (id, external_id, first_name, last_name, email)
 VALUES
     ('94833b54-132b-4ab8-b841-60df45809b3e', 'ad:001', 'Ville', 'Virkailija', 'ville@noreplytest.fi');
