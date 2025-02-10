ae64a4417aed4f8776022f36e1568b172c8d354c
Samuli Toivonen <samuli.toivonen@reaktor.com>
2025-02-10 11:20:13 +0200

    Form component label translation updates

--- frontend/src/lib-components/form/utils.ts
+++ frontend/src/lib-components/form/utils.ts
@@ -1,3 +1,4 @@
+import { Translations } from 'citizen-frontend/localization'
 import { BoundForm } from 'lib-common/form/hooks'
 import { AnyForm } from 'lib-common/form/types'
 
@@ -13,4 +14,11 @@ export function bindOrPlaceholders<T extends AnyForm>(bind?: BoundForm<T>) {
     validationError: () => '',
     translateError: () => ''
   }
+}
+
+export function getI18nLabel(
+  labelValue: string | ((i18n: Translations) => string) | undefined,
+  i18n: Translations
+) {
+  return labelValue instanceof Function ? labelValue(i18n) : labelValue
 }
