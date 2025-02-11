de3430d06b9b545ad6b80004f5a5be67049e0f43
Joonas Reynders <joonas.reynders@reaktor.fi>
2025-02-11 09:41:18 +0200

    Bump kotlin gradle spring plugin

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "2.0.20"
-    kotlin("plugin.spring") version "1.9.20"
+    kotlin("plugin.spring") version "1.9.25"
     id("org.flywaydb.flyway") version "11.3.1"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
     id("com.github.node-gradle.node") version "7.1.0"
