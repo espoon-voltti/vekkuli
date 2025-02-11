40dc374566e7dc8b3e6d97745263fa08699a07e5
Joonas Reynders <joonas.reynders@reaktor.fi>
2025-02-11 10:13:57 +0200

    Bump jackson xml

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -92,7 +92,7 @@ dependencies {
     implementation("org.jdbi:jdbi3-kotlin")
     implementation("org.jdbi:jdbi3-postgres")
 
-    api(platform("com.fasterxml.jackson:jackson-bom:2.18.0"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.18.2"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     implementation("com.auth0:java-jwt:4.5.0")
