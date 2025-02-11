054daeeb355176f8c5a742ddd671f1490c3476b3
Joonas Reynders <joonas.reynders@reaktor.fi>
2025-02-11 19:00:46 +0200

    Bump spring boot

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-    id("org.springframework.boot") version "3.2.0"
+    id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "2.0.20"
     kotlin("plugin.spring") version "1.9.25"
@@ -98,7 +98,11 @@ dependencies {
     implementation("com.auth0:java-jwt:4.5.0")
 
     implementation("net.logstash.logback:logstash-logback-encoder:7.4")
-    implementation("ch.qos.logback:logback-access")
+    api("ch.qos.logback.access:logback-access-tomcat:2.0.5")
+
+    // These constraints are needed for CVE fixes
+    api("ch.qos.logback:logback-classic:1.5.15")
+    api("ch.qos.logback:logback-core:1.5.15")
     api("io.github.oshai:kotlin-logging-jvm:7.0.3")
 
     testImplementation(kotlin("test"))
