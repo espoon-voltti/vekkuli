8000ad8c17e73c5693af6328b1603056e7500abd
Joonas Reynders <joonas.reynders@reaktor.fi>
2025-02-12 09:23:11 +0200

    Bump versions

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     kotlin("jvm") version "2.0.20"
     kotlin("plugin.spring") version "1.9.25"
     id("org.flywaydb.flyway") version "11.3.1"
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
     id("com.github.node-gradle.node") version "7.1.0"
     kotlin("plugin.serialization") version "2.0.20"
     id("org.owasp.dependencycheck") version "12.0.1"
