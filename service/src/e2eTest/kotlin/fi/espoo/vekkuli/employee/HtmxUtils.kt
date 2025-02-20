8896f583ad16b4eba0bd7acb6459a625975c5f9c
Jari Helenius <68942246+juhenius@users.noreply.github.com>
2025-02-20 17:04:01 +0200

    Improve HTMX related flakiness in E2E tests (#831)

--- service/src/e2eTest/kotlin/fi/espoo/vekkuli/employee/HtmxUtils.kt
+++ service/src/e2eTest/kotlin/fi/espoo/vekkuli/employee/HtmxUtils.kt
@@ -3,18 +3,21 @@ package fi.espoo.vekkuli.employee
 import com.microsoft.playwright.Locator
 import com.microsoft.playwright.Page
 
-fun Page.waitForHtmxSettle() {
-    waitForFunction(
-        """() => new Promise(resolve => {
-            document.body.addEventListener("htmx:afterSettle", (event) => {
-                resolve(true);
-            }, { once: true });
-            htmx.process(document.body);
-        })"""
+private val htmxSettled = "htmxHasSettled"
+
+fun Page.waitForHtmxSettle(action: Page.() -> Unit) {
+    evaluate(
+        """
+        window.$htmxSettled = 'false';
+        window.addEventListener("htmx:afterSettle", (event) => window.$htmxSettled = true, { once: true });
+        """.trimIndent()
     )
+    action()
+    waitForFunction("window.$htmxSettled === true")
 }
 
 fun Locator.clickAndWaitForHtmxSettle() {
-    click()
-    page().waitForHtmxSettle()
+    page().waitForHtmxSettle {
+        click()
+    }
 }
--- service/src/e2eTest/kotlin/fi/espoo/vekkuli/employee/ReserveBoatSpaceAsEmployeeTest.kt
+++ service/src/e2eTest/kotlin/fi/espoo/vekkuli/employee/ReserveBoatSpaceAsEmployeeTest.kt
@@ -1072,9 +1072,7 @@ class ReserveBoatSpaceAsEmployeeTest : ReserveTest() {
         formPage.submitButton.click()
 
         formPage.existingCitizenSelector.click()
-        "olivia".forEach { character ->
-            formPage.citizenSearchInput.press("$character")
-        }
+        typeText(formPage.citizenSearchInput, "olivia")
         page.waitForCondition { formPage.citizenSearchOption1.isVisible() }
         formPage.citizenSearchOption1.click()
 
@@ -1183,7 +1181,6 @@ class ReserveBoatSpaceAsEmployeeTest : ReserveTest() {
         val formPage = BoatSpaceFormPage(page)
         formPage.existingCitizenSelector.click()
         typeText(formPage.citizenSearchInput, "olivia")
-        page.waitForHtmxSettle()
         page.waitForCondition { formPage.citizenSearchOption1.isVisible }
         formPage.citizenSearchOption1.clickAndWaitForHtmxSettle()
 
@@ -1221,7 +1218,6 @@ class ReserveBoatSpaceAsEmployeeTest : ReserveTest() {
         val formPage = BoatSpaceFormPage(page)
         formPage.existingCitizenSelector.click()
         typeText(formPage.citizenSearchInput, "olivia")
-        page.waitForHtmxSettle()
         page.waitForCondition { formPage.citizenSearchOption1.isVisible }
         formPage.citizenSearchOption1.clickAndWaitForHtmxSettle()
 
