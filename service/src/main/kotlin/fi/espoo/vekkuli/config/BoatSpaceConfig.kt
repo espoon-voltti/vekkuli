068db22d5b0dfe48da9367fcb0d0be3eb54453f8
Väinö Jalonen <vaino.jalonen@reaktor.com>
2025-02-20 10:13:16 +0200

    Change session timeout to 40 minutes

--- service/src/main/kotlin/fi/espoo/vekkuli/config/BoatSpaceConfig.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/config/BoatSpaceConfig.kt
@@ -30,7 +30,7 @@ enum class ReservationWarningType {
 }
 
 object BoatSpaceConfig {
-    const val SESSION_TIME_IN_SECONDS = 20 * 60
+    const val SESSION_TIME_IN_SECONDS = 40 * 60
     const val BOAT_RESERVATION_ALV_PERCENTAGE = 25.5
 
     fun paytrailProductCode(product: BoatSpaceType): String {
