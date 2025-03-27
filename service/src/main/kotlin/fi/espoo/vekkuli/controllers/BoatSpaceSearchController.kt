e6726fa2692e316f776d49237d630159e9bcb16f
Väinö Jalonen <vaino.jalonen@reaktor.com>
2025-03-27 12:53:43 +0200

    Remove usertype

--- service/src/main/kotlin/fi/espoo/vekkuli/controllers/BoatSpaceSearchController.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/controllers/BoatSpaceSearchController.kt
@@ -5,7 +5,6 @@ import fi.espoo.vekkuli.config.BoatSpaceConfig.winterStorageLocations
 import fi.espoo.vekkuli.config.audit
 import fi.espoo.vekkuli.config.getAuthenticatedUser
 import fi.espoo.vekkuli.controllers.Routes.Companion.USERTYPE
-import fi.espoo.vekkuli.controllers.Utils.Companion.getCitizen
 import fi.espoo.vekkuli.controllers.Utils.Companion.getServiceUrl
 import fi.espoo.vekkuli.controllers.Utils.Companion.isAuthenticated
 import fi.espoo.vekkuli.domain.BoatSpaceAmenity
@@ -27,7 +26,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
@@ -67,74 +65,46 @@ class BoatSpaceSearchController {
 
     @RequestMapping("/$USERTYPE/venepaikat")
     @ResponseBody
-    fun boatSpaceSearchPage(
-        request: HttpServletRequest,
-        @PathVariable usertype: String
-    ): ResponseEntity<String> {
+    fun boatSpaceSearchPage(request: HttpServletRequest,): ResponseEntity<String> {
         request.getAuthenticatedUser()?.let {
             logger.audit(it, "BOAT_SPACE_SEARCH")
         }
-        val userType = UserType.fromPath(usertype)
-        if (userType == UserType.EMPLOYEE) {
-            val authenticatedUser = request.getAuthenticatedUser() ?: return ResponseEntity(HttpStatus.FORBIDDEN)
-            val user =
-                authenticatedUser.let {
-                    jdbi.inTransactionUnchecked { tx ->
-                        tx.getAppUser(authenticatedUser.id)
-                    }
+        val authenticatedUser = request.getAuthenticatedUser() ?: return ResponseEntity(HttpStatus.FORBIDDEN)
+        val user =
+            authenticatedUser.let {
+                jdbi.inTransactionUnchecked { tx ->
+                    tx.getAppUser(authenticatedUser.id)
                 }
-            if (user == null) {
-                return ResponseEntity(HttpStatus.FORBIDDEN)
             }
-            val reservation = reservationService.getUnfinishedReservationForEmployee(user.id)
-            if (reservation != null) {
-                val headers = org.springframework.http.HttpHeaders()
-                val url =
-                    if (reservation.status == ReservationStatus.Payment) {
-                        "/${userType.path}/venepaikka/varaus/${reservation.id}/lasku"
-                    } else {
-                        "/${userType.path}/venepaikka/varaus/${reservation.id}"
-                    }
-                headers.location = URI(getServiceUrl(url))
-                return ResponseEntity(headers, HttpStatus.FOUND)
-            }
-            val locations = reservationService.getHarbors()
-
-            return ResponseEntity.ok(
-                employeeLayout.render(
-                    true,
-                    request.requestURI,
-                    boatSpaceSearch.render(locations, winterStorageLocations, true)
-                )
-            )
+        if (user == null) {
+            return ResponseEntity(HttpStatus.FORBIDDEN)
         }
-        val citizen = getCitizen(request, reserverService)
-        if (citizen != null) {
-            val reservation =
-                reservationService.getUnfinishedReservationForCitizen(citizen.id)
-
-            if (reservation != null) {
-                val headers = org.springframework.http.HttpHeaders()
-                headers.location = URI(getServiceUrl("/${userType.path}/venepaikka/varaus/${reservation.id}"))
-                return ResponseEntity(headers, HttpStatus.FOUND)
-            }
+        val reservation = reservationService.getUnfinishedReservationForEmployee(user.id)
+        if (reservation != null) {
+            val headers = org.springframework.http.HttpHeaders()
+            val url =
+                if (reservation.status == ReservationStatus.Payment) {
+                    "/virkailija/venepaikka/varaus/${reservation.id}/lasku"
+                } else {
+                    "/virkailija/venepaikka/varaus/${reservation.id}"
+                }
+            headers.location = URI(getServiceUrl(url))
+            return ResponseEntity(headers, HttpStatus.FOUND)
         }
         val locations = reservationService.getHarbors()
 
         return ResponseEntity.ok(
-            layout.render(
-                isAuthenticated(userType, request),
-                citizen?.fullName,
+            employeeLayout.render(
+                true,
                 request.requestURI,
-                boatSpaceSearch.render(locations, winterStorageLocations)
+                boatSpaceSearch.render(locations, winterStorageLocations, true)
             )
         )
     }
 
-    @RequestMapping("/$USERTYPE/partial/vapaat-paikat")
+    @RequestMapping("/virkailija/partial/vapaat-paikat")
     @ResponseBody
     fun searchResultPartial(
-        @PathVariable usertype: String,
         @RequestParam(required = false) boatType: BoatType?,
         @RequestParam @Min(0) width: BigDecimal?,
         @RequestParam @Min(0) length: BigDecimal?,
@@ -150,7 +120,6 @@ class BoatSpaceSearchController {
                 "BOAT_SPACE_SEARCH_RESULTS"
             )
         }
-        val userType = UserType.fromPath(usertype)
 
         val harbors =
             boatSpaceService.getUnreservedBoatSpaceOptions(
@@ -166,8 +135,7 @@ class BoatSpaceSearchController {
             harbors.first,
             BoatFilter(width, length, boatType),
             harbors.second,
-            isAuthenticated(userType, request),
-            userType == UserType.EMPLOYEE
+            isAuthenticated(UserType.EMPLOYEE, request),
         )
     }
 }
