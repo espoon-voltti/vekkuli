45b7ca4a419c4ab7c593877b15c5840e6ca314ad
Jari Helenius <jari.helenius@reaktor.com>
2025-04-03 18:46:39 +0300

    Fix redis connection in tests

--- service/src/main/kotlin/fi/espoo/vekkuli/config/RedisConfig.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/config/RedisConfig.kt
@@ -3,19 +3,15 @@ package fi.espoo.vekkuli.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig {
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory()
-
-    @Bean
-    fun redisTemplate(): RedisTemplate<String, String> {
+    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
         val template = RedisTemplate<String, String>()
-        template.setConnectionFactory(redisConnectionFactory())
+        template.setConnectionFactory(connectionFactory)
         template.keySerializer = StringRedisSerializer()
         template.valueSerializer = StringRedisSerializer()
         return template
