04ba1a615a2f8ca37a12a117b7e95f85d17a049d
Joonas Reynders <joonas.reynders@reaktor.fi>
2025-05-14 15:03:05 +0300

    Remove white spaces from emails and phone numbers

--- frontend/src/citizen-frontend/citizen/sections/citizenInformation/formDefinitions.ts
+++ frontend/src/citizen-frontend/citizen/sections/citizenInformation/formDefinitions.ts
@@ -1,13 +1,14 @@
 import { User } from 'citizen-frontend/auth/state'
-import { string } from 'lib-common/form/fields'
+import { whitespaceTrimmedString } from 'lib-common/form/fields'
 import { object, required, validated } from 'lib-common/form/form'
 import { email, phone } from 'lib-common/form/form-validation'
 import { StateOf } from 'lib-common/form/types'
 
 export const citizenInformationForm = object({
-  email: validated(required(string()), email),
-  phone: validated(required(string()), phone)
+  email: validated(required(whitespaceTrimmedString()), email),
+  phone: validated(required(whitespaceTrimmedString()), phone)
 })
+
 export type CitizenInformationForm = typeof citizenInformationForm
 
 export function initialFormState(user: User): StateOf<CitizenInformationForm> {
