f25e7676385f69085fdb703967a7316a29b31c97
Joonas Reynders <joonas.reynders@reaktor.fi>
2025-05-15 13:04:45 +0300

    Tests for phone number and email whitespace trimming

--- service/src/main/kotlin/fi/espoo/vekkuli/boatSpace/organization/OrganizationDetailsView.kt
+++ service/src/main/kotlin/fi/espoo/vekkuli/boatSpace/organization/OrganizationDetailsView.kt
@@ -37,7 +37,7 @@ class OrganizationDetailsView(
                     ${commonComponents.goBackButton(backUrl)} 
                     <h2 class='mb-none'>${organization.name}</h2>
                 </div>
-                <div class='container block'>
+                <div class='container block' data-test-id="organization-details">
                     <h3>${t("organizationDetails.title.organizationInformation")}</h3>
                     <div id="organization-information">
                       ${organizationContactDetails.render(organization, organizationMembers)}
