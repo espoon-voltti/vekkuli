2c1d7f73b82221c12ea3bc884b14577c70edf2fa
Joonas Reynders <joonas.reynders@iki.fi>
2026-01-13 13:52:51 +0200

    Sähköpostimuutokset, osa 3 (#1880)
    * Bryggplats -> båtplats
    
    * Fix text Din bokning är giltig till tills vidare...
    
    * More text fixes
    
    * More text changes
    
    * Spot, slip, space -> place
    
    * Space -> place

--- service/src/main/resources/db/migration/R__email_templates.sql
+++ service/src/main/resources/db/migration/R__email_templates.sql
@@ -45,7 +45,7 @@ Platsens bredd: {{width}}
 Platsens längd: {{length}}
 Platsens utrustning/förvaringssätt: {{amenitySv}}
 
-Din bokning är i kraft till {{endDateSv}}.
+Din bokning är {{endDateSv}}.
 
 Du får senare per post en bokningsbekräftelse och ett säsongsklistermärke. I bokningsbekräftelsen finns en nyckelkod som du behöver för att tillverka en portnyckel till bryggan (brygga F i Björnviken har ingen port).
 
@@ -76,11 +76,11 @@ Width: {{width}}
 Length: {{length}}
 Amenities/storage type: {{amenityEn}}
 
-Your reservation is valid until {{endDateEn}}.
+Your reservation is valid {{endDateEn}}.
 
 You will later receive by mail a reservation confirmation and a season sticker, which includes the key code for having a key made for the pier gate (note: there is no gate at Otsolahti’s Fpier).
 
-If you reserved a storage space in Ämmäsmäki:
+If you reserved a storage place in Ämmäsmäki:
 The pickup of the access badge must be arranged in advance by calling 04040 634 3463 on weekdays between 8-14. The badge is picked up from the Suomenoja harbor (Hylkeenpyytäjäntie 9) by showing the payment receipt.
 
 Manage your reservations, boats and personal details easily at https://varaukset.espoo.fi/kuntalainen/omat-tiedot.
@@ -129,7 +129,7 @@ venepaikat@espoo.fi
 
 Bästa kund,
 
-Vi har bokat en  {{placeTypeSv}} {{name}} från Esbo stad till dig.
+Vi har bokat en {{placeTypeSv}} {{name}} från Esbo stad till dig.
 
 Fakturan skickas till {{invoiceAddressSv}}. För att bekräfta bokningen, betala platsen senast på förfallodatumet. Om betalningen uteblir annulleras bokningen och platsen blir tillgänglig för andra. När betalningen har mottagits på vårt konto skickar vi information om nyckeln till bryggans port samt klistermärket för din båt per post.
 
@@ -143,9 +143,9 @@ Platsens bredd: {{width}}
 Platsens längd: {{length}}
 Platsens utrustning/förvaringssätt: {{amenitySv}}
 
-Bokningen gäller till {{endDateSv}}.
+Bokningen gäller {{endDateSv}}.
 
-Om du har bokat en bryggplats får du senare per post ett sänsongklistermärke för din båt och en hamnkarta med nyckelkodsuppgifter för att tillverka en nyckel till bryggans port (obs! Ingen port till F-bryggan i Björnviken). Klistermärket ska fästas synligt på båten eller, om du har bokat en vinter- eller förvaringsplats, på skyddstältet, trailern eller bocken.
+Om du har bokat en båtplats får du senare per post ett sänsongklistermärke för din båt och en hamnkarta med nyckelkodsuppgifter för att tillverka en nyckel till bryggans port (obs! Ingen port till F-bryggan i Björnviken). Klistermärket ska fästas synligt på båten eller, om du har bokat en vinter- eller förvaringsplats, på skyddstältet, trailern eller bocken.
 
 Om du har bokat en förvaringsplats i Käringbacken:
 För att få tillgång till en elektronisk portnyckel ska man i förväg komma överrens om upphämtning av nyckeln på telefonnummer 046 877 3668 (Toimela) eller 044 566 8690 (Pohjala) vardagar mellan 8-14. Upphämtning av nyckeln från Finno hamn (Hylkeenpyytäjäntie 9) får du genoma att visa kvittot på den betalda platsen.
@@ -164,7 +164,7 @@ Dear customer,
 
 You have reserved a {{placeTypeEn}} {{name}} from the City of Espoo.
 
-The invoice will be sent to {{invoiceAddressEn}}. To confirm your booking, please pay for the spot before the due date. If unpaid, the booking will be canceled and made available for others. Once payment is received, we will send additional information about the dock gate key via email and the season sticker by mail.
+The invoice will be sent to {{invoiceAddressEn}}. To confirm your booking, please pay for the place before the due date. If unpaid, the booking will be canceled and made available for others. Once payment is received, we will send additional information about the dock gate key via email and the season sticker by mail.
 
 Tenant:
 {{reserverName}}
@@ -176,11 +176,11 @@ Width: {{width}}
 Length: {{length}}
 Amenities/storage type: {{amenityEn}}
 
-Your reservation is valid until {{endDateEn}}.
+Your reservation is valid {{endDateEn}}.
 
-If you reserved a dock space, you will receive a season sticker and a harbor map by mail later, containing the key code needed for making a key for the dock gate (Otsolahti F-dock has no gate). The season sticker must be placed visibly on the boat, or if you reserved a winter or storage space, on the protective tent, trailer or stand.
+If you reserved a dock place, you will receive a season sticker and a harbor map by mail later, containing the key code needed for making a key for the dock gate (Otsolahti F-dock has no gate). The season sticker must be placed visibly on the boat, or if you reserved a winter or storage place, on the protective tent, trailer or stand.
 
-If you reserved a storage space in Ämmäsmäki:
+If you reserved a storage place in Ämmäsmäki:
 The collection of the access tag for Ämmämäki must be arranged in advance by calling tel. 040 634 3463 on weekdays between 8:00 and 14:00. The access tag is collected from the Suomenoja harbor (Hylkeenpyytäjäntie 9) upon presentation of the payment receipt.
 
 Manage your reservations, boats and personal details easily at https://varaukset.espoo.fi/kuntalainen/omat-tiedot.
@@ -339,7 +339,7 @@ Dear customer,
 
 Your reservation for {{placeTypeEn}} has been extended for a new season.
 
-The invoice will be sent to {{invoiceAddressEn}}. To confirm your reservation, please pay for the spot before the due date. An unpaid spot will be canceled and made available for others.
+The invoice will be sent to {{invoiceAddressEn}}. To confirm your reservation, please pay for the place before the due date. An unpaid place will be canceled and made available for others.
 
 Tenant:
 {{reserverName}}
@@ -455,7 +455,7 @@ Olet vaihtanut Espoon kaupungilta vuokraamaasi {{placeTypeFi}}a.
 
 HUOM! Vanha paikka päättyy heti vaihdon yhteydessä ja vesikulkuneuvo tulee siirtää pois vanhalta paikalta, sillä vanha paikkasi on vapautunut seuraavalle vuokrattavaksi. Venepaikkasi vuokrakausi säilyy ennallaan.
 
-Saat myöhemmin postissa varausvahvistuksen ja kausitarran, jossa avainkoodi laiturin portin avaimen teettämistä varten (Otsolahden F-laiturille ei ole porttia). Kausitarra tulee kiinnittää näkyvälle paikalle vesikulkuneuvoon tai talvi- ja säilytyspaikalla, suojatelttaan, traileriin tai pukkiin.{{citizenReserverFi}}
+Saat myöhemmin postissa kausitarran ja varausvahvistuksen, jossa avainkoodi laiturin portin avaimen teettämistä varten (Otsolahden F-laiturille ei ole porttia).{{citizenReserverFi}}
 
 Uusi paikka:
 
@@ -487,7 +487,7 @@ Du har bytt din hyrda {{placeTypeSv}} i Esbo stad.
 
 OBS! Den gamla platsen upphör genast i samband med bytet och vattenfarkosten ska flyttas från den gamla platsen, eftersom den gamla platsen har blivit ledig att hyras ut till nästa kund. Hyresperioden för din båtplats förblir oförändrad.
 
-Du får senare per post en bokningsbekräftelse och ett säsongsklistermärke. I bokningsbekräftelsen finns en nyckelkod som du behöver för att tillverka en portnyckel till bryggan (brygga F i Björnviken har ingen port). Säsongsklistermärket ska fästas på ett synligt ställe på vattenfarkosten eller på vinter-, trailer- eller förvaringsplatsen på skyddstält, trailer eller bock. {{citizenReserverSv}}
+Du får senare per post en bokningsbekräftelse och ett säsongsklistermärke. I bokningsbekräftelsen finns en nyckelkod som du behöver för att tillverka en portnyckel till bryggan (brygga F i Björnviken har ingen port).{{citizenReserverSv}}
 
 Ny plats:
 
@@ -519,7 +519,7 @@ You have changed your rented place in the City of Espoo.
 
 NOTICE! The previous place ends immediately upon the change, and the watercraft must be removed from the old place, as it has been released for rental to the next user. Your rental period remains unchanged.
 
-You will later receive by mail a reservation confirmation and a season sticker, which includes the key code for having a key made for the pier gate (note: there is no gate at Otsolahti’s F pier). The season sticker must be affixed in a visible place on the watercraft, or at the winter and storage place — on the protective tent, trailer, or cradle.
+You will later receive by mail a reservation confirmation and a season sticker, which includes the key code for having a key made for the pier gate (note: there is no gate at Otsolahti’s F pier).
 
 New location:
 
@@ -598,11 +598,11 @@ Terminator: {{terminatorName}}
 
 Tenant: {{reserverName}}
 
-The space must be empty and cleaned for the next tenant.
+The place must be empty and cleaned for the next tenant.
 
 Upon termination, the right to the place has ceased, and the premises must be vacated without delay.
 
-If you have terminated a storage space in Ämmäsmäki:
+If you have terminated a storage place in Ämmäsmäki:
 The access badge must be returned to the Finno harbor (Hylkeenpyytäjäntie 9).
 
 Best regards,
@@ -706,9 +706,9 @@ Amenities/storage type: {{amenityEn}}
 
 Your reservation is valid until {{endDateEn}}.
 
-If you reserved a dock space, you will receive a season sticker and a harbor map by mail later, containing the key code needed for making a key for the dock gate (Otsolahti F-dock has no gate). The season sticker must be placed visibly on the boat, or if you reserved a winter or storage space, on the protective tent, trailer or stand.
+If you reserved a dock place, you will receive a season sticker and a harbor map by mail later, containing the key code needed for making a key for the dock gate (Otsolahti F-dock has no gate). The season sticker must be placed visibly on the boat, or if you reserved a winter or storage place, on the protective tent, trailer or stand.
 
-If you reserved a storage space in Ämmäsmäki:
+If you reserved a storage place in Ämmäsmäki:
 The pickup of the access badge must be arranged in advance by calling 046 877 3668 (Toimela) or 044 566 8690 (Pohjala) on weekdays between 8-14. The badge is picked up from the Suomenoja harbor (Hylkeenpyytäjäntie 9) by showing the payment receipt.
 
 Manage your reservations, boats and personal details easily at https://varaukset.espoo.fi/kuntalainen/omat-tiedot.
@@ -764,9 +764,9 @@ Your reservation for {{placeTypeEn}} {{name}} in the City of Espoo is coming to 
 
 Tenant: {{reserverName}}
 
-Once the rental period ends, the space must be emptied and cleaned.
+Once the rental period ends, the place must be emptied and cleaned.
 
-If you rented a storage space in Ämmäsmäki, the access badge must be returned to the Finno harbor (Hylkeenpyytäjäntie 9).
+If you rented a storage place in Ämmäsmäki, the access badge must be returned to the Finno harbor (Hylkeenpyytäjäntie 9).
 
 You can check reservation periods and make a new reservation at https://varaukset.espoo.fi.
 
@@ -827,7 +827,7 @@ Du gör det enkelt på https://varaukset.espoo.fi/kuntalainen/omat-tiedot. Du ka
 Om säsongsavgiften inte har betalats senast {{endDate}}, sägs platsen upp och blir tillgänglig för andra.
 
 Tider för förnyelse av plats:
-Bryggplats: 7.–31.1.
+Båtplats: 7.–31.1.
 Trailerplats: 1.–30.4.
 Vinterplats: 15.8.–14.9.
 Förvaringsplats i Käringbacken: 15.8.–14.9.
@@ -861,18 +861,18 @@ Dear customer,
 
 It is time to renew your reservation for {{placeTypeEn}} for the next season.
 
-Secure your spot by paying the seasonal fee.
+Secure your place by paying the seasonal fee.
 You can do this easily at https://varaukset.espoo.fi/kuntalainen/omat-tiedot. You will find the seasonal payment button under your reservation details. Online banking credentials are required for payment.
 
-If the seasonal fee is not paid by {{endDate}}, your spot will be canceled and made available for others.
+If the seasonal fee is not paid by {{endDate}}, your place will be canceled and made available for others.
 
-Deadlines for renewing a spot:
-Dock space: 7.–31.1.
-Trailer space: 1.–30.4.
+Deadlines for renewing a place:
+Dock place: 7.–31.1.
+Trailer place: 1.–30.4.
 Winter storage: 15.8.–14.9.
-Storage space in Ämmäsmäki: 15.8.–14.9.
+Storage place in Ämmäsmäki: 15.8.–14.9.
 
-Spot to be renewed:
+Place to be renewed:
 
 Tenant: {{reserverName}}
 
@@ -883,7 +883,7 @@ Width: {{width}}
 Length: {{length}}
 Amenities/storage type: {{amenityEn}}
 
-Please note that if your home municipality is no longer Espoo, you may not be eligible to renew your spot.
+Please note that if your home municipality is no longer Espoo, you may not be eligible to renew your place.
 
 If you are unable to make the payment, please contact us via email at venepaikat@espoo.fi or by phone at 09 81658984. Phone hours can be found on our website.
 
@@ -954,7 +954,7 @@ The rental agreement for the {{placeTypeEn}} {{name}} has ended.
 
 Tenant: {{reserverName}}
 
-Once the rental period ends, the space must be emptied and cleaned.
+Once the rental period ends, the place must be emptied and cleaned.
 
 You can check reservation periods and make a new reservation at https://varaukset.espoo.fi.
 
